4382fbf33c154851f097372f60587539a4412be6
Muhammad Mubashir Khan <mubashir.khan@rainsoftfn.com>
2020-05-27 15:07:17 +0500

    removed keys to partial

--- Rainmaker2/src/MicroServices/KeyStore/KeyStore.Service/AESCryptography.cs
+++ Rainmaker2/src/MicroServices/KeyStore/KeyStore.Service/AESCryptography.cs
@@ -4,12 +4,8 @@ using System.Text;
 
 namespace KeyStore.Service
 {
-    public class AESCryptography
+    public partial class AESCryptography
     {
-        private const string AesIV256 = @"!QAZ2WSX#EDC4RFV";
-        private const string AesKey256 = @"5TGB&YHN7UJM(IK<5TGB&YHN7UJM(IK<";
-
-
         public static string Encrypt(string text)
         {
             // AesCryptoServiceProvider
---
+++ Rainmaker2/src/MicroServices/KeyStore/KeyStore.Service/AESKeys.cs
@@ -0,0 +1,8 @@
+﻿namespace KeyStore.Service
+{
+    public partial class AESCryptography
+    {
+        private const string AesIV256 = @"!QAZ2WSX#EDC4RFV";
+        private const string AesKey256 = @"5TGB&YHN7UJM(IK<5TGB&YHN7UJM(IK<";
+    }
+}
