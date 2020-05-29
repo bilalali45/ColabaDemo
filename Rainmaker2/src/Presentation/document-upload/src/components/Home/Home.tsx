39fd627cdb8f764ca68b3f75ab9d8b5b67147446
Muhammad Usman <iam.usmansarwar@gmailo.com>
2020-05-29 18:18:03 +0500

    bootstarp packeges

--- Rainmaker2/src/Presentation/document-upload/src/components/Home/Home.tsx
+++ Rainmaker2/src/Presentation/document-upload/src/components/Home/Home.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import { ActivityHeader } from './AcitivityHeader/ActivityHeader'
+// import { ActivityHeader } from './AcitivityHeader/ActivityHeader'
 import { Activity } from './Activity/Activity'
 import { DocumentRequest } from './DocumentRequest/DocumentRequest'
 import { UploadedDocuments } from './UploadedDocuments/UploadedDocuments'
-import ImageAssets from '../../utils/image_assets/ImageAssets'
+// import ImageAssets from '../../utils/image_assets/ImageAssets'
 
 import Header from '../../shared/Components/Header/Header'
 import Footer from '../../shared/Components/Footer/Footer'
@@ -13,33 +13,14 @@ export const Home = () => {
     return (
         <div>
             <Header />
+
+            <div className="content">
+                Hello World
+            </div>
             
             <Footer />
 
-            /* <div className="alert alert-primary" role="alert">
-            A simple primary alert—check it out!
-            </div>
-            <div className="alert alert-secondary" role="alert">
-            A simple secondary alert—check it out!
-            </div>
-            <div className="alert alert-success" role="alert">
-            A simple success alert—check it out!
-            </div>
-            <div className="alert alert-danger" role="alert">
-            A simple danger alert—check it out!
-            </div>
-            <div className="alert alert-warning" role="alert">
-            A simple warning alert—check it out!
-            </div>
-            <div className="alert alert-info" role="alert">
-            A simple info alert—check it out!
-            </div>
-            <div className="alert alert-light" role="alert">
-            A simple light alert—check it out!
-            </div>
-            <div className="alert alert-dark" role="alert">
-            A simple dark alert—check it out!
-            </div> 
+            
 
             <Switch>
                 <Route path="/home/activity" component={Activity}/>
--- Rainmaker2/src/Presentation/document-upload/src/shared/Components/Header/Header.tsx
+++ Rainmaker2/src/Presentation/document-upload/src/shared/Components/Header/Header.tsx
@@ -4,7 +4,43 @@ import ImageAssets from '../../../utils/image_assets/ImageAssets';
 const Header = () => {
     return (
         <header className="header-main">
-            <img src={ImageAssets.footer.logo512} alt=""/>
+            
+ 
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-12">
+                        <nav className="navbar navbar-default">
+                            <div className="navbar-header">
+                                <a className="navbar-brand" href="/">
+
+<img alt="Texas Trust Home Loans - Mortgage Lender in Texas " src={ImageAssets.header.logoheader} className="d-none d-sm-block" />
+                        </a> 
+                    </div>
+                                    <div className="s-account pull-right">
+
+                                        <a className="d-name d-none d-sm-block" href="/Dashboard">
+                                            Hello,
+                                            Muhammad Usman
+                        </a>
+                                        <div className="dropdown">
+                                            <a className="dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+
+                                                <span>
+                                                    MU
+                            </span>
+                                            </a>
+                                            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                <a className="dropdown-item" href="/Dashboard">Dashboard</a>
+                                                <a className="dropdown-item" href="/Account/ManagePassword">Change Password</a>
+                                                <a className="dropdown-item" href="/Account/LogOff">Sign Out</a>
+                                            </div>
+                                        </div>
+                                    </div>
+            </nav>
+                            </div>
+    </div>
+                    </div>
+
         </header>
     )
 }
