897b8285419764068128d07b6d90e7d506d89bb5
Muhammad Mubashir Khan <mubashir.khan@rainsoftfn.com>
2020-05-29 10:39:34 +0500

    added default controller

---
+++ Rainmaker2/src/APIGateways/MainGateway/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet("[action]")]
+        [Route("/")]
+        public string Index()
+        {
+            return "Main Gateway is running";
+        }
+    }
+}
--- Rainmaker2/src/MicroServices/Identity/Controllers/TokenController.cs
+++ Rainmaker2/src/MicroServices/Identity/Controllers/TokenController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Identity.Controllers
 {
-    [Route("api/token")]
+    [Route("api/[controller]")]
     [ApiController]
     public class TokenController : Controller
     {
