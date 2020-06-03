ab503fdd08fe49e7419aa7e4dc0c239fb5f67bf3
Zohaib Siddiqui <zohaib.siddiqui@rainsoftfn.com>
2020-06-03 17:44:01 +0500

    Document Request

--- Rainmaker2/src/Presentation/document-upload/src/components/Home/Activity/DocumentsStatus/DocumentStatus.tsx
+++ Rainmaker2/src/Presentation/document-upload/src/components/Home/Activity/DocumentsStatus/DocumentStatus.tsx
@@ -1,25 +1,33 @@
 import React from 'react'
-import arrowForward from './../../../../assets/images/arrow-forward.svg';
+//import arrowForward from './../../../../assets/images/arrow-forward.svg';
+import {SVG} from './../../../../shared/Components/Assets/SVG';
 
 export const DocumentStatus = () => {
     return (
         <div className="DocumentStatus box-wrap">
-            <div className="DocumentStatus--header">
-                <h2 className="heading-h2">Document Request</h2>
-                <p>You have <span className="DocumentStatus--count">8</span> items to complete</p>
-            </div>
-            <div className="DocumentStatus--body">
-                <ul className="list">
-                    <li>Bank Statement</li>
-                    <li>W-2s 2017</li>
-                    <li>W-2s 2018</li>
-                    <li>Personal Tax Returns</li>
-                </ul>
-                <div>
-                    <a href="" className="DocumentStatus--get-link">Show 4 more Tasks <img src={arrowForward} alt="go" /></a>
+            <div className="row">
+                <div className="col-md-7 DocumentStatus--left-side">
+                    <div className="DocumentStatus--header">
+                        <h2 className="heading-h2">Document Request</h2>
+                        <p>You have <span className="DocumentStatus--count">8</span> items to complete</p>
+                    </div>
+                    <div className="DocumentStatus--body">
+                        <ul className="list">
+                            <li>Bank Statement</li>
+                            <li>W-2s 2017</li>
+                            <li>W-2s 2018</li>
+                            <li>Personal Tax Returns</li>
+                        </ul>
+                        <div>
+                            <a href="" className="DocumentStatus--get-link">Show 4 more Tasks <SVG  shape="arrowFarword"/></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-5 DocumentStatus--right-side">
+                    <SVG shape="storage"/>
+                    <button className="btn btn-primary float-right">Get Started</button>
                 </div>
             </div>
-
         </div>
     )
 }
