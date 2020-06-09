11247bbb073e75bf7c99bc313d3d52bf57c0dfb8
Muhammad Usman <iam.usmansarwar@gmailo.com>
2020-06-09 20:26:45 +0500

    new design changes

--- Rainmaker2/src/Presentation/document-upload/src/components/Home/AcitivityHeader/ActivityHeader.tsx
+++ Rainmaker2/src/Presentation/document-upload/src/components/Home/AcitivityHeader/ActivityHeader.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import { LoanStatus } from '../Activity/LoanStatus/LoanStatus'
 const ActivityHeader = () => {
     return (
         <div className="activityHeader">
-            <h1>Activity Header</h1>
+            <section className="compo-loan-status">
+            <LoanStatus />
+            </section>
+            <section className="row-subheader">
+            <div className="row">
+    <div className="container">
+            <div className="row sub-header-wrap">
+                <div className="col-6">
+                Dashboard
+                </div>
+                <div className="col-6 text-right">
+                Uploaded Document
+                </div>
+            </div>
+            </div>
+            </div>
+            </section>
+            
         </div>
     )
 }
