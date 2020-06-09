3d4442f9644aa76f52a721e9b0ace4cb5105e72e
Muhammad Mubashir Khan <mubashir.khan@rainsoftfn.com>
2020-06-09 17:42:35 +0500

    added new attributes

--- Rainmaker2/src/MicroServices/DocumentManagement/DocumentManagement.Entity/RequestFile.cs
+++ Rainmaker2/src/MicroServices/DocumentManagement/DocumentManagement.Entity/RequestFile.cs
@@ -8,9 +8,11 @@ namespace DocumentManagement.Entity
     {
         public string clientName { get; set; }
         public string serverName { get; set; }
+        public string borrowerName { get; set; }
         public DateTime fileUploadedOn { get; set; }
         public int size { get; set; }
         public string encryptionKey { get; set; }
         public string encryptionAlgorithm { get; set; }
+        public int order { get; set; }
     }
 }
