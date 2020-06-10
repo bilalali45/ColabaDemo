207910a0eb6ba69864d5d46ac5225793904d5c14
Talhan Tayyab <talha.tayyab@rainsoftfn.com>
2020-06-10 10:52:36 +0500

    Add File Service

---
+++ Rainmaker2/src/MicroServices/DocumentManagement/DocumentManagement.Service/FileService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DocumentManagement.Service
+{
+    public class FileService : IFileService
+    {
+        private readonly IMongoService mongoService;
+
+        public FileService(IMongoService mongoService)
+        {
+            this.mongoService = mongoService;
+        }
+    }
+}
