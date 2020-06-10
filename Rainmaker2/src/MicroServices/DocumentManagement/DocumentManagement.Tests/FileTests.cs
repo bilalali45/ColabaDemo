3dc4c9a0be5d4434cb6cdb28a4e9b9ef3fa91529
Talhan Tayyab <talha.tayyab@rainsoftfn.com>
2020-06-10 16:30:56 +0500

    rename service test cases

--- Rainmaker2/src/MicroServices/DocumentManagement/DocumentManagement.Tests/FileTests.cs
+++ Rainmaker2/src/MicroServices/DocumentManagement/DocumentManagement.Tests/FileTests.cs
@@ -13,6 +13,7 @@ using Moq.Protected;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
+using System.IO;
 
 namespace DocumentManagement.Tests
 {
@@ -23,7 +24,7 @@ namespace DocumentManagement.Tests
         {
             //Arrange
             Mock<IFileService> mock = new Mock<IFileService>();
-            FileRenameModel model = new FileRenameModel() { docId = "1" , requestId = "1" , fileId = "1" , fileName = "clientName.txt" };
+            FileRenameModel model = new FileRenameModel() { docId = "1", requestId = "1", fileId = "1", fileName = "clientName.txt" };
 
             mock.Setup(x => x.Rename(It.IsAny<FileRenameModel>())).ReturnsAsync(true);
 
@@ -49,6 +50,50 @@ namespace DocumentManagement.Tests
             //Assert
             Assert.NotNull(result);
             Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task TestRenameServiceTrue()
+        {
+            //Arrange
+            Mock<IMongoService> mock = new Mock<IMongoService>();
+            Mock<IMongoDatabase> mockdb = new Mock<IMongoDatabase>();
+            Mock<IMongoCollection<Request>> mockCollection = new Mock<IMongoCollection<Request>>();
+
+            var fileRenameModel = new FileRenameModel() { id= "5eb25d1fe519051af2eeb72d", docId = "5eb25d1fe519051af2eeb72d", requestId = "5eb25d1fe519051af2eeb72d", fileId = "5eb25d1fe519051af2eeb72d", fileName = "clientName.txt" };
+            mockdb.Setup(x => x.GetCollection<Request>(It.IsAny<string>(), It.IsAny<MongoCollectionSettings>())).Returns(mockCollection.Object);
+            mockCollection.Setup(x => x.UpdateOneAsync(It.IsAny<FilterDefinition<Request>>(), It.IsAny<UpdateDefinition<Request>>(),It.IsAny<UpdateOptions>(), It.IsAny<CancellationToken>())).ReturnsAsync(new UpdateResult.Acknowledged(1,1, BsonInt32.Create(1)));
+            mock.SetupGet(x => x.db).Returns(mockdb.Object);
+
+            //Act
+
+            IFileService fileService = new FileService(mock.Object);
+            bool result = await fileService.Rename(fileRenameModel);
+
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async Task TestRenameServiceFalse()
+        {
+            //Arrange
+            Mock<IMongoService> mock = new Mock<IMongoService>();
+            Mock<IMongoDatabase> mockdb = new Mock<IMongoDatabase>();
+            Mock<IMongoCollection<Request>> mockCollection = new Mock<IMongoCollection<Request>>();
+
+            var fileRenameModel = new FileRenameModel() { id = "5eb25d1fe519051af2eeb72d", docId = "5eb25d1fe519051af2eeb72d", requestId = "5eb25d1fe519051af2eeb72d", fileId = "5eb25d1fe519051af2eeb72d", fileName = "clientName.txt" };
+            mockdb.Setup(x => x.GetCollection<Request>(It.IsAny<string>(), It.IsAny<MongoCollectionSettings>())).Returns(mockCollection.Object);
+            mockCollection.Setup(x => x.UpdateOneAsync(It.IsAny<FilterDefinition<Request>>(), It.IsAny<UpdateDefinition<Request>>(), It.IsAny<UpdateOptions>(), It.IsAny<CancellationToken>())).ReturnsAsync(new UpdateResult.Acknowledged(0, 0, BsonInt32.Create(1)));
+            mock.SetupGet(x => x.db).Returns(mockdb.Object);
+
+            //Act
+
+            IFileService fileService = new FileService(mock.Object);
+            bool result = await fileService.Rename(fileRenameModel);
+
+            //Assert
+            Assert.False(result);
         }
     }
 }
