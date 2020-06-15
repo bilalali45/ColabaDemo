3b678a24b955776be9cfe461fd0b5bbb8b5f9f93
Muhammad Mubashir Khan <mubashir.khan@rainsoftfn.com>
2020-06-15 09:54:28 +0500

    finalized ket store

--- Rainmaker2/src/MicroServices/KeyStore/KeyStore.Console/Program.cs
+++ Rainmaker2/src/MicroServices/KeyStore/KeyStore.Console/Program.cs
@@ -10,21 +10,35 @@ namespace KeyStore.Console
             while(true)
             {
                 System.Console.WriteLine();
-                System.Console.WriteLine("Enter 1 to encrypt, 2 to decrypt, any other key to quit");
+                System.Console.WriteLine("Enter 1 to encrypt key, 2 to decrypt key, 3 to encrypt data, 4 to decrypt data, any other key to quit");
                 System.Console.WriteLine();
                 string value = System.Console.ReadLine();
-                string key;
+                string key,data;
                 switch (value)
                 {
                     case "1":
                         System.Console.WriteLine("Enter key");
                         key = System.Console.ReadLine();
-                        System.Console.WriteLine(AESCryptography.Encrypt(key));
+                        System.Console.WriteLine(AESCryptography.Encrypt(key, AESCryptography.AesKey256));
                         break;
                     case "2":
                         System.Console.WriteLine("Enter key");
                         key = System.Console.ReadLine();
-                        System.Console.WriteLine(AESCryptography.Decrypt(key));
+                        System.Console.WriteLine(AESCryptography.Decrypt(key, AESCryptography.AesKey256));
+                        break;
+                    case "3":
+                        System.Console.WriteLine("Enter key");
+                        key = System.Console.ReadLine();
+                        System.Console.WriteLine("Enter data");
+                        data = System.Console.ReadLine();
+                        System.Console.WriteLine(AESCryptography.Encrypt(data, key));
+                        break;
+                    case "4":
+                        System.Console.WriteLine("Enter key");
+                        key = System.Console.ReadLine();
+                        System.Console.WriteLine("Enter data");
+                        data = System.Console.ReadLine();
+                        System.Console.WriteLine(AESCryptography.Decrypt(data, key));
                         break;
                     default:
                         return;
