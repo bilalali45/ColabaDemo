86456beb1bdabdaa83c8e37ba421ac838f19a681
Danish Iqbal <danish.iqbal@rainsoftfn.com>
2020-06-16 07:42:35 -0700

    fixed issues with auth

--- Rainmaker2/src/Presentation/document-upload/src/components/Home/Home.tsx
+++ Rainmaker2/src/Presentation/document-upload/src/components/Home/Home.tsx
@@ -29,6 +29,7 @@ export const Home = () => {
         if (!authenticate() || !Auth.getLoanAppliationId() || !Auth.getBusinessUnitId() || !Auth.getBusinessUnitId()) {
             history.push('/Account/Login');
         }
+        setCookie('Rainmaker2Token', Auth.checkAuth());
     }, []);
 
 
@@ -39,12 +40,11 @@ export const Home = () => {
                 Auth.saveAuth(cookies.Rainmaker2Token);
                 return true;
             } else {
-                UserActions.authenticate();
+                // UserActions.authenticate();
             }
         }
         return true;
     }
-
 
     return (
         <div>
