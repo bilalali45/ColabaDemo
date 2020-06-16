4d8657f061ff07d85a700f60c20db3b5cc033d34
Muhammad Usman <iam.usmansarwar@gmailo.com>
2020-06-16 19:53:04 +0500

    LOAN PROGRESS integration

--- Rainmaker2/src/Presentation/document-upload/src/components/Home/Activity/LoanProgress/LoanProgress.tsx
+++ Rainmaker2/src/Presentation/document-upload/src/components/Home/Activity/LoanProgress/LoanProgress.tsx
@@ -2,19 +2,68 @@ import React, { useEffect, useState } from 'react';
 import { SVGchecked } from './../../../../shared/Components/Assets/SVG';
 import Carousel from 'react-bootstrap/Carousel'
 import { DocumentActions } from '../../../../store/actions/DocumentActions';
+
+import Lpstep1 from '../../../../assets/images/lp-step1.svg';
+import Lpstep2 from '../../../../assets/images/lp-step2.svg';
+import Lpstep3 from '../../../../assets/images/lp-step3.svg';
+import Lpstep4 from '../../../../assets/images/lp-step4.svg';
+import Lpstep5 from '../../../../assets/images/lp-step5.svg';
+import { Activity } from '../Activity';
+
+
 type Props = {
     //userName: string,
 }
 
+
+
 export const LoanProgress: React.SFC<Props> = (props) => {
-    const [index, setIndex] = useState(2);
     const [loanProgress, setLoanProgress] = useState([]);
+    
+    const [currentItem, setCurrentItem] = useState<any>({});
+    const [index, setIndex] = useState(0);
+   
+
+    const loanProgressImages = [
+        {
+            order: 1,
+            img: Lpstep1
+        },
+        {
+            order: 2,
+            img: Lpstep2
+        },
+        {
+            order: 3,
+            img: Lpstep3
+        },
+        {
+            order: 4,
+            img: Lpstep4
+        },
+        {
+            order: 5,
+            img: Lpstep5
+        },
+    ]
+
+
 
     useEffect(() => {
         if (!loanProgress.length) {
             fetchLoanProgress();
         }
+        let activeStep: any = loanProgress.find((l: any) => l.isCurrentStep);
+        setCurrentItem(activeStep);
     });
+
+    useEffect(() => {
+        let activeStep: any = loanProgress.find((l: any) => l.isCurrentStep);
+        if(activeStep){
+            let a = activeStep.order - 1
+            setIndex(a)
+        }
+    },[currentItem]);
 
     const fetchLoanProgress = async () => {
         let applicationId = localStorage.getItem('loanApplicationId');
@@ -58,7 +107,7 @@ export const LoanProgress: React.SFC<Props> = (props) => {
             return (
                 <Carousel.Item>
                     <div className="lp-list">
-                        <div className="step-count">{l.order}</div>
+                        <div className="step-count"><img src={loanProgressImages[i].img} alt={l.order} /></div>
                         <div className="lp-content">
                             <div className="step-label">{l.status}</div>
                             <h6>{l.name}</h6>
@@ -73,13 +122,16 @@ export const LoanProgress: React.SFC<Props> = (props) => {
 
     const renderCarouselList = () => {
         var totallist = loanProgress.length;
+        var id = index;
+        // debugger
         console.log(loanProgress)
         return loanProgress.map((l: any, i: number) => {
             let liclass = "completed-icon";
             liclass = l.status == 'In progress' ? 'current-icon' : l.status == 'To be done' ? 'upcoming-icon' : 'completed-icon';
             let step = i + 1;
+            var activeindex = i === id ? " active" : ""
             return (
-                <li data-index={i} className={liclass}>
+                <li data-index={activeindex} className={liclass + activeindex}>
                     <a href="javascrit:" onClick={(e) => handleSelect(i, e)}>
                         {i == totallist - 1 && l.status == 'To be done' ? <i className="zmdi zmdi-flag"></i> : l.status == 'Completed' ? <i className="zmdi zmdi-check"></i> : l.status == 'In progress' ? <i className="zmdi zmdi-male-alt"></i> : <span>{step}</span>}
                     </a>
@@ -89,13 +141,18 @@ export const LoanProgress: React.SFC<Props> = (props) => {
         })
     }
 
+
+
+
+
+
     return (
         <div className="LoanProgress box-wrap">
             <div className="box-wrap--header">
                 <h2 className="heading-h2"> Your Loan Progress </h2>
             </div>
             <div className="box-wrap--body">
-                <div className={index == 4 || index == 3 ? "lp-wrap upcoming-step" : index == 2 ? "lp-wrap current-step" : "lp-wrap"}>
+                <div className={index == currentItem?.order - 1 ? "lp-wrap current-step" : index > currentItem?.order - 1 ? "lp-wrap upcoming-step" : "lp-wrap"}>
                     <div className="list-wrap">
                         {
                             renderCarousel()
@@ -103,14 +160,8 @@ export const LoanProgress: React.SFC<Props> = (props) => {
                     </div>
                     <div className="lp-footer">
                         <ul>
-<<<<<<< HEAD
                             {renderCarouselList()}
                             {/* <li className={index == 0 ? "completed-icon active" : "completed-icon"}>
-=======
-
-                            
-                            <li className={index == 0 ? "completed-icon active" : "completed-icon"}>
->>>>>>> c495e9c8db1280b685a8e748a390e1ee951ea326
                                 <a href="javascrit:" onClick={(e) => handleSelect(0, e)}>
                                     <i className="zmdi zmdi-check"></i>
                                 </a>
