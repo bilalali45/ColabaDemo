4b1f2243eb28f4173097bde243a6c96fa81456b5
Jehangir Babul <jehangir.babul@rainsoftfn.com>
2020-06-24 12:09:22 +0500

    Added moment lib

--- Rainmaker2/src/Presentation/document-upload/src/components/Home/UploadedDocuments/UploadedDocumentsTable/UploadedDocumentsTable.tsx
+++ Rainmaker2/src/Presentation/document-upload/src/components/Home/UploadedDocuments/UploadedDocumentsTable/UploadedDocumentsTable.tsx
@@ -2,54 +2,67 @@ import React, { useState, useEffect } from 'react'
 import { UploadedDocuments } from '../../../../entities/Models/UploadedDocuments'
 import { DocumentActions } from '../../../../store/actions/DocumentActions';
 import { Auth } from '../../../../services/auth/Auth';
-export const UploadedDocumentsTable = () => {
-    
-const [docList, setDocList] = useState<UploadedDocuments[] | null>(null)
+import { Document } from '../../../../entities/Models/Document'
+import moment from 'moment';
+import * as momentDate from 'moment';
 
- useEffect(()=>{
-     if(!docList?.length){
-        fetchUploadedDocuments();
-     }
-   
- },[]);    
-    
-  const fetchUploadedDocuments = async () => {
-   let uploadedDocs = await DocumentActions.getSubmittedDocuments(Auth.getLoanAppliationId(), Auth.getTenantId());
-   console.log('uploadedDocs',uploadedDocs)
-   if(uploadedDocs){
-    setDocList(uploadedDocs);
-   }
-  }  
-    
-    
-    
+export const UploadedDocumentsTable = () => {
+
+    const [docList, setDocList] = useState<UploadedDocuments[] | null>(null)
+
+    useEffect(() => {
+        if (!docList?.length) {
+            fetchUploadedDocuments();
+        }
+    }, []);
+
+    const fetchUploadedDocuments = async () => {
+        let uploadedDocs = await DocumentActions.getSubmittedDocuments(Auth.getLoanAppliationId(), Auth.getTenantId());
+        console.log('uploadedDocs', uploadedDocs)
+        if (uploadedDocs) {
+            setDocList(uploadedDocs);
+        }
+    }
+    const renderFileNameColumn = (data) => {
+        return <td>
+            {data.map((item: Document) => {
+                return <tr><a href="" className="block-element">{item.clientName}</a></tr>
+            })}
+        </td>
+    }
+
+    const renderAddedColumn = (data) => {
+        return <td>
+            {data.map((item: Document) => {
+                return <tr><span className="block-element">{
+                    moment(new Date(item.fileUploadedOn)).format('MM-DD-YYYY HH:mm')
+                    }
+                    
+                    </span></tr>
+            })}
+        </td>
+    }
+
     const renderUploadedDocs = (data) => {
-        return data.map((item: any) => {
-            debugger
-            console.log('item',item)
+        return data.map((item: UploadedDocuments) => {
+            if (!item.files.length)
+                return;
             return (
                 <tbody>
                     <tr>
-                        <td><em className="far fa-file"></em> Bank Statement</td>
-                        <td>
-                            <a href="" className="block-element">Bank-statement-jan-to-mar-2020-1.jpg</a>
-                            <a href="" className="block-element">Bank-statement-jan-to-mar-2020-2.jpg</a>
-                            <a href="" className="block-element">Bank-statement-jan-to-mar-2020-3.jpg</a>
-                        </td>
-                        <td>
-                            <span className="block-element">12-04-2020 17:30</span>
-                            <span className="block-element">12-04-2020 17:30</span>
-                            <span className="block-element">12-04-2020 17:30</span>
-                        </td>
-                    </tr>
+                        <td><em className="far fa-file"></em> {item.docName}</td>
+                        {renderFileNameColumn(item.files)}
+                        {renderAddedColumn(item.files)}
 
+                    </tr>
                 </tbody>
             )
         })
     }
-    
+
     return (
         <div className="UploadedDocumentsTable">
+          {docList &&
             <table className="table  table-striped">
                 <thead>
                     <tr>
@@ -58,10 +71,14 @@ const [docList, setDocList] = useState<UploadedDocuments[] | null>(null)
                         <th>Added</th>
                     </tr>
                 </thead>
-               { docList &&
-               renderUploadedDocs(docList)
-               } 
+                
+                    {renderUploadedDocs(docList)}
+               
             </table>
+             }
+             {!docList &&
+            <div>No document</div>
+             }
         </div>
     )
 }
