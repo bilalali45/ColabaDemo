4ec58b63bbbfaf1b86296ec935e2a2d782920558
Zohaib Siddiqui <zohaib.siddiqui@rainsoftfn.com>
2020-06-24 13:40:35 +0500

    mergaes

--- Rainmaker2/src/Presentation/document-upload/src/components/Home/UploadedDocuments/UploadedDocumentsTable/UploadedDocumentsTable.tsx
+++ Rainmaker2/src/Presentation/document-upload/src/components/Home/UploadedDocuments/UploadedDocumentsTable/UploadedDocumentsTable.tsx
@@ -3,14 +3,10 @@ import { UploadedDocuments } from '../../../../entities/Models/UploadedDocuments
 import { DocumentActions } from '../../../../store/actions/DocumentActions';
 import { Auth } from '../../../../services/auth/Auth';
 import { Document } from '../../../../entities/Models/Document'
-<<<<<<< HEAD
 import moment from 'moment';
 import * as momentDate from 'moment';
-//import DocUploadIcon from '../../../assets/images/upload-doc-icon.svg';
 import DocUploadIcon from '../../../../assets/images/upload-doc-icon.svg';
-=======
 import { DateFormat } from '../../../../utils/helpers/DateFormat';
->>>>>>> 2227fc01d9a647e9d610bff7f3176a9431eaabbb
 
 export const UploadedDocumentsTable = () => {
 
@@ -40,18 +36,8 @@ export const UploadedDocumentsTable = () => {
     const renderAddedColumn = (data) => {
         return <td>
             {data.map((item: Document) => {
-<<<<<<< HEAD
-                return (
-                    <span className="block-element">{
-                        moment(new Date(item.fileUploadedOn)).format('MM-DD-YYYY HH:mm')
-                        }
-                        
-                        </span>
-                )
-=======
                 return <tr><span className="block-element">{DateFormat(item.fileUploadedOn, true)}           
                     </span></tr>
->>>>>>> 2227fc01d9a647e9d610bff7f3176a9431eaabbb
             })}
         </td>
     }
@@ -89,25 +75,21 @@ export const UploadedDocumentsTable = () => {
                
             </table>
              }
-<<<<<<< HEAD
-             {!docList &&
-            <div className="no-document">               
+            
+             {!docList?.length &&
+                <div className="no-document">               
 
-                 <div className="no-document--wrap">
-                        <div className="no-document--img">
-                            <img src={DocUploadIcon} alt="Your don't have any files!" />
-                        </div>
-                        <label htmlFor="inputno-document--text">
-                            Your don't have any files.<br/>
-                            Go to <a href="http://localhost:3000/DocumentManagement/uploadedDocuments">Loan Home</a>
-                        </label>
-                </div>  
-                
-            </div>
-=======
-             {docList?.length === 0 &&
-            <div>No document</div>
->>>>>>> 2227fc01d9a647e9d610bff7f3176a9431eaabbb
+                <div className="no-document--wrap">
+                       <div className="no-document--img">
+                           <img src={DocUploadIcon} alt="Your don't have any files!" />
+                       </div>
+                       <label htmlFor="inputno-document--text">
+                           Your don't have any files.<br/>
+                           Go to <a href="http://localhost:3000/DocumentManagement/uploadedDocuments">Loan Home</a>
+                       </label>
+               </div>  
+               
+           </div>
              }
         </div>
     )
