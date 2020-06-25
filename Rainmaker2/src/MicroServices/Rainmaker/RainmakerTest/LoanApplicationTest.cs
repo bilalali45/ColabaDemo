223734e9c794ae80a97126ebc58ba590f437065c
Shehroz Reaz <shehroz.reaz@rainsoftfn.com>
2020-06-25 05:18:31 -0700

    Test case completed

--- Rainmaker2/src/MicroServices/Rainmaker/RainmakerTest/LoanApplicationTest.cs
+++ Rainmaker2/src/MicroServices/Rainmaker/RainmakerTest/LoanApplicationTest.cs
@@ -21,6 +21,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using System.Linq;
+using System;
 
 namespace RainmakerTest
 {
@@ -185,7 +186,7 @@ namespace RainmakerTest
             Assert.Equal("Test", res.PropertyType);
         }
         [Fact]
-        public async Task TestGetLOInfoController()
+        public async Task TestGetLOInfoIsNotNullController()
         {
             //Arrange
             Mock<ILoanApplicationService> mock = new Mock<ILoanApplicationService>();
@@ -200,7 +201,6 @@ namespace RainmakerTest
             loanApplicationController.ControllerContext = context;
 
             LoanOfficer obj = new LoanOfficer() { FirstName = "Smith" };
-
             mock.Setup(x => x.GetLOInfo(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>())).ReturnsAsync(obj);
 
             //Act
@@ -212,7 +212,62 @@ namespace RainmakerTest
             Assert.NotNull(content);
             Assert.Equal("Smith", content.FirstName);
         }
+        [Fact]
+        public async Task TestGetLOInfoIsnullController()
+        {
+            //Arrange
+            Mock<ILoanApplicationService> mock = new Mock<ILoanApplicationService>();
 
+            var loanApplicationController = new LoanApplicationController(mock.Object, null, null);
+
+            var httpContext = new Mock<HttpContext>();
+            httpContext.Setup(m => m.User.FindFirst("UserProfileId")).Returns(new Claim("UserProfileId", "1"));
+
+            var context = new ControllerContext(new ActionContext(httpContext.Object, new RouteData(), new ControllerActionDescriptor()));
+
+            loanApplicationController.ControllerContext = context;
+
+            LoanOfficer obj = new LoanOfficer() { FirstName = null };
+
+            mock.Setup(x => x.GetLOInfo(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>())).ReturnsAsync(obj);
+
+            //Act
+            IActionResult result = await loanApplicationController.GetLOInfo(1, 1);
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<OkObjectResult>(result);
+            var content = (result as OkObjectResult).Value as LoanOfficer;
+            Assert.Null(content);
+           // Assert.Null(content.FirstName);
+        }
+        [Fact]
+        public async Task TestGetLOInfoIsnullObjController()
+        {
+            //Arrange
+            Mock<ILoanApplicationService> mock = new Mock<ILoanApplicationService>();
+
+            var loanApplicationController = new LoanApplicationController(mock.Object, null, null);
+
+            var httpContext = new Mock<HttpContext>();
+            httpContext.Setup(m => m.User.FindFirst("UserProfileId")).Returns(new Claim("UserProfileId", "1"));
+
+            var context = new ControllerContext(new ActionContext(httpContext.Object, new RouteData(), new ControllerActionDescriptor()));
+
+            loanApplicationController.ControllerContext = context;
+
+            LoanOfficer obj = null;
+
+            mock.Setup(x => x.GetLOInfo(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>())).ReturnsAsync(obj);
+
+            //Act
+            IActionResult result = await loanApplicationController.GetLOInfo(1, 1);
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<OkObjectResult>(result);
+            var content = (result as OkObjectResult).Value as LoanOfficer;
+            Assert.Null(content);
+            // Assert.Null(content.FirstName);
+        }
         [Fact]
         public async Task GetLOInfo()
         {
@@ -420,7 +475,7 @@ namespace RainmakerTest
 
             mockcommonservice.Setup(x => x.GetSettingFreshValueByKeyAsync<string>(SystemSettingKeys.FtpEmployeePhotoFolder, businessUnitId, default)).ReturnsAsync(string.Empty);
             //  mockftpservice.Setup(x => x.DownloadStream(It.IsAny<string>())).ReturnsAsync(new MemoryStream());
-            mockftpservice.Setup(x => x.DownloadStream(It.IsAny<string>())).ReturnsAsync(It.IsAny<Stream>());
+            mockftpservice.Setup(x => x.DownloadStream(It.IsAny<string>())).Throws(new Exception(""));
 
             LoanApplicationController controller = new LoanApplicationController(null, mockcommonservice.Object, mockftpservice.Object);
 
@@ -434,7 +489,7 @@ namespace RainmakerTest
 
             Assert.IsType<string>(result);
 
-            Assert.Equal("/9j/4QAYRXhpZgAASUkqAAgAAAAAAAAAAAAAAP/sABFEdWNreQABAAQAAAA8AAD/4QMvaHR0cDovL25zLmFkb2JlLmNvbS94YXAvMS4wLwA8P3hwYWNrZXQgYmVnaW49Iu+7vyIgaWQ9Ilc1TTBNcENlaGlIenJlU3pOVGN6a2M5ZCI/PiA8eDp4bXBtZXRhIHhtbG5zOng9ImFkb2JlOm5zOm1ldGEvIiB4OnhtcHRrPSJBZG9iZSBYTVAgQ29yZSA1LjYtYzE0MiA3OS4xNjA5MjQsIDIwMTcvMDcvMTMtMDE6MDY6MzkgICAgICAgICI+IDxyZGY6UkRGIHhtbG5zOnJkZj0iaHR0cDovL3d3dy53My5vcmcvMTk5OS8wMi8yMi1yZGYtc3ludGF4LW5zIyI+IDxyZGY6RGVzY3JpcHRpb24gcmRmOmFib3V0PSIiIHhtbG5zOnhtcD0iaHR0cDovL25zLmFkb2JlLmNvbS94YXAvMS4wLyIgeG1sbnM6eG1wTU09Imh0dHA6Ly9ucy5hZG9iZS5jb20veGFwLzEuMC9tbS8iIHhtbG5zOnN0UmVmPSJodHRwOi8vbnMuYWRvYmUuY29tL3hhcC8xLjAvc1R5cGUvUmVzb3VyY2VSZWYjIiB4bXA6Q3JlYXRvclRvb2w9IkFkb2JlIFBob3Rvc2hvcCBDQyAyMDE4IChXaW5kb3dzKSIgeG1wTU06SW5zdGFuY2VJRD0ieG1wLmlpZDpCOTNCNjI5QkI0NjUxMUVBQjA4MUI3NzVGMjkyRDYwOSIgeG1wTU06RG9jdW1lbnRJRD0ieG1wLmRpZDpCOTNCNjI5Q0I0NjUxMUVBQjA4MUI3NzVGMjkyRDYwOSI+IDx4bXBNTTpEZXJpdmVkRnJvbSBzdFJlZjppbnN0YW5jZUlEPSJ4bXAuaWlkOkI5M0I2Mjk5QjQ2NTExRUFCMDgxQjc3NUYyOTJENjA5IiBzdFJlZjpkb2N1bWVudElEPSJ4bXAuZGlkOkI5M0I2MjlBQjQ2NTExRUFCMDgxQjc3NUYyOTJENjA5Ii8+IDwvcmRmOkRlc2NyaXB0aW9uPiA8L3JkZjpSREY+IDwveDp4bXBtZXRhPiA8P3hwYWNrZXQgZW5kPSJyIj8+/+4ADkFkb2JlAGTAAAAAAf/bAIQABgQEBAUEBgUFBgkGBQYJCwgGBggLDAoKCwoKDBAMDAwMDAwQDA4PEA8ODBMTFBQTExwbGxscHx8fHx8fHx8fHwEHBwcNDA0YEBAYGhURFRofHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8f/8AAEQgARgBGAwERAAIRAQMRAf/EAG0AAQEAAwEBAAAAAAAAAAAAAAAGBAUHAwgBAQEAAAAAAAAAAAAAAAAAAAABEAABAwMBBQYFBQAAAAAAAAABAAIDEQQFEiExQYEGYXEyktITUaEiUxeRQmIUFREBAAAAAAAAAAAAAAAAAAAAAP/aAAwDAQACEQMRAD8A+qUBAQEBAQEBAQEBAQEBAQEBAQEBAQT/AFH1dBin/wBWGMXF6RqcwmjIwdxeRxPABBORdf5tsuqSOCRnGMNc3Z2Oq5BZYPOWmXtTNBVkjDpmhd4mO5bweBQbFAQEBAQEHIcpLJLlL2SSpe6eTVXseWgcgKKKxkFH0DLI3OvjbXRJA73Bw+lzdJ5VP6ojoioICAgIBIAqdyDneexMN/fXN9g5G3rNVbu3iNXsfxe0fva7+PH5Bo48fkZZPajtJ3SbtAjfXnUbOairLo+xscZdyw3VxH/szMFbYOBMcY26KjYXne4Ds71UVyAgICAgiOus9KZjiLd2mNoBvHDe4uFRH3U2u+O74oJCKSSKRskL3RSM8L2EtcO4ihUVmyZ7OSR+2+/nLN1A/SfM2h+aDBFWkFpLXNOprgaEOG2oI49qDpPSGdflLBzLg1vLYhsrt2tp8L+dKHtVRvkBAQEE7ddDYq5uprmWe4MkzzI+jmUq48Po3IPL8fYb71z5o/Qgfj7DfeufNH6ED8fYb71z5o/Qgz8N0vYYm5fcW0sznSM9tzZHNLaVBrQNbt2INwgICAgICAgICAgICAgICAgICAgIP//Z", result);
+
         }
 
 
@@ -521,7 +576,7 @@ namespace RainmakerTest
             Assert.Equal("shehroz@gmail.com", res.Email);
             Assert.Equal("Shehroz", res.FirstName);
             Assert.Equal("", res.LastName);
-            Assert.Null( res.NMLS);
+            Assert.Null(res.NMLS);
             Assert.Equal("030012345678", res.Phone);
             Assert.Equal("", res.Photo);
             Assert.Equal("https://entityframeworkcore.com//lo/Shehroz", res.WebUrl);
