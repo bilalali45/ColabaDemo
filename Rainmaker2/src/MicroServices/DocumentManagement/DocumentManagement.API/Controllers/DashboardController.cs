3f8317e3eb9e8bb5dd53ad75af2f84b9e88bbd79
Muhammad Mubashir Khan <mubashir.khan@rainsoftfn.com>
2020-06-26 09:58:04 +0500

    allow anonymous removed

--- Rainmaker2/src/MicroServices/DocumentManagement/DocumentManagement.API/Controllers/DashboardController.cs
+++ Rainmaker2/src/MicroServices/DocumentManagement/DocumentManagement.API/Controllers/DashboardController.cs
@@ -41,7 +41,6 @@ namespace DocumentManagement.API.Controllers
             int userProfileId = int.Parse(User.FindFirst("UserProfileId").Value.ToString());
             return Ok(await dashboardService.GetDashboardStatus(loanApplicationId,tenantId,userProfileId));
         }
-        [AllowAnonymous]
         [HttpGet("GetFooterText")]
         public async Task<IActionResult> GetFooterText(int tenantId,int businessUnitId)
         {
