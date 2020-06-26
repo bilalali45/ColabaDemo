0adf4e7f26518c6f77a1712581527e8fdd90e2db
Talhan Tayyab <talha.tayyab@rainsoftfn.com>
2020-06-26 17:43:12 +0500

    added admin dashboard controller and getmenu API.

---
+++ Rainmaker2/src/MicroServices/Rainmaker/Rainmaker.Service/IUserProfileService.cs
@@ -0,0 +1,14 @@
+﻿using RainMaker.Entity.Models;
+using RainMaker.Service;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rainmaker.Service
+{
+    public interface IUserProfileService : IServiceBase<UserProfile>
+    {
+        Task<UserProfile> GetUserProfile(int userProfileId);
+    }
+}
---
+++ Rainmaker2/src/MicroServices/Rainmaker/RainmakerTest/AdminDashboardTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Moq;
+using Rainmaker.API.Controllers;
+using Rainmaker.Service;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Routing;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Rainmaker.Test
+{
+    public class AdminDashboardTest
+    {
+        [Fact]
+        public async Task TestGetMenuController()
+        {
+            //Arrange
+            Mock<IUserProfileService> mockUserProfileService = new Mock<IUserProfileService>();
+            Mock<ISitemapService> mockSitemapService = new Mock<ISitemapService>();
+
+            var adminDashboardController = new AdminDashboardController(mockSitemapService.Object, mockUserProfileService.Object);
+
+            var httpContext = new Mock<HttpContext>();
+            httpContext.Setup(m => m.User.FindFirst("UserProfileId")).Returns(new Claim("UserProfileId", "1"));
+
+            var context = new ControllerContext(new ActionContext(httpContext.Object, new Microsoft.AspNetCore.Routing.RouteData(), new ControllerActionDescriptor()));
+
+            adminDashboardController.ControllerContext = context;
+
+            //LoanOfficer obj = new LoanOfficer() { FirstName = "Smith" };
+            //mockSitemapService.Setup(x => x.Get(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>())).ReturnsAsync(obj);
+
+            ////Act
+            //IActionResult result = await loanApplicationController.GetLOInfo(1, 1);
+            ////Assert
+            //Assert.NotNull(result);
+            //Assert.IsType<OkObjectResult>(result);
+            //var content = (result as OkObjectResult).Value as LoanOfficer;
+            //Assert.NotNull(content);
+            //Assert.Equal("Smith", content.FirstName);
+        }
+    }
+}
