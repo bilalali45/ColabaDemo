8cd44310c06c3365b3ae9ed96fca2cad22978b1c
Talhan Tayyab <talha.tayyab@rainsoftfn.com>
2020-07-01 10:43:33 +0500

    create template controller,service,interface and model.

---
+++ Rainmaker2/src/MicroServices/DocumentManagement/DocumentManagement.Service/ITemplateService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using static DocumentManagement.Model.Template;
+
+namespace DocumentManagement.Service
+{
+    public interface ITemplateService
+    {
+        Task<List<TemplateModel>> GetTemplates(int? tenantId, int userProfileId);
+    }
+}
