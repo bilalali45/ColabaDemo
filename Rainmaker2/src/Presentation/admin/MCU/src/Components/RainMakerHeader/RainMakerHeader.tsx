c295a2fd5d1a538eee922f9491ac698768021f92
Zohaib Siddiqui <zohaib.siddiqui@rainsoftfn.com>
2020-07-01 17:42:14 +0500

    update

--- Rainmaker2/src/Presentation/admin/MCU/src/Components/RainMakerHeader/RainMakerHeader.tsx
+++ Rainmaker2/src/Presentation/admin/MCU/src/Components/RainMakerHeader/RainMakerHeader.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const RainMakerHeader = () => {
     return (
-        <div>
-            <h1>RainMakerHeader</h1>
-        </div>
+        <header className="rainmaker-header">
+            cvxcvxzcvxcv
+        </header>
     )
 }
