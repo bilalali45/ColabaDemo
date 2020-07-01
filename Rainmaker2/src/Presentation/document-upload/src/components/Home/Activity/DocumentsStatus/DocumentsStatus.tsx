6881b5a153df7d99ae2fab8bb90ca17f3b981327
Muhammad Usman <iam.usmansarwar@gmailo.com>
2020-07-01 14:45:59 +0500

    Activity dashboard doc request finished doc UI

--- Rainmaker2/src/Presentation/document-upload/src/components/Home/Activity/DocumentsStatus/DocumentsStatus.tsx
+++ Rainmaker2/src/Presentation/document-upload/src/components/Home/Activity/DocumentsStatus/DocumentsStatus.tsx
@@ -4,6 +4,7 @@ import { DocumentActions } from '../../../../store/actions/DocumentActions';
 import { Endpoints } from '../../../../store/endpoints/Endpoints';
 import { Auth } from '../../../../services/auth/Auth';
 import IconEmptyDocRequest from '../../../../assets/images/empty-doc-req-icon.svg';
+import IconDoneDocRequest from '../../../../assets/images/done-doc-req-icon.svg';
 import { Store } from '../../../../store/store';
 import { DocumentsActionType } from '../../../../store/reducers/documentReducer';
 import { DocumentRequest } from '../../../../entities/Models/DocumentRequest';
@@ -54,6 +55,32 @@ export const DocumentsStatus = () => {
                             <div className="eds-txt">
                                 <p>
                                     You have 0 task to complete
+                            </p>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+
+    const renderCompletedDocs = () => {
+        return (
+            <div className="DocumentStatus box-wrap empty">
+                <div className="box-wrap--header clearfix">
+                    <h2 className="heading-h2"> Document Request</h2>
+                </div>
+                <div className="box-wrap--body clearfix">
+                    <div className="edc-flex">
+                        <div className="eds-wrap">
+                            <div className="eds-img">
+                                <img src={IconDoneDocRequest} alt="" />
+                            </div>
+                            <div className="eds-txt">
+                                <p>
+                                <span className="text-primary">Great!</span> You have done all the tasks
                             </p>
 
                             </div>
