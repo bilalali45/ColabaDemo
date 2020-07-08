be134fea218a54716111721b1d55c661a1724767
Jehangir Babul <jehangir.babul@rainsoftfn.com>
2020-07-08 13:32:24 +0500

    Added toTitleCase library

--- Rainmaker2/src/Presentation/libraries/rainsoft-js/testing.js
+++ Rainmaker2/src/Presentation/libraries/rainsoft-js/testing.js
@@ -4,3 +4,4 @@ var index_1 = require("./index");
 console.log(index_1.MaskPhone(2254165226));
 console.log(index_1.UnMaskPhone("(258) 896-454"));
 console.log(index_1.FormatAmountByCountry(250000.56));
+console.log((index_1.toTitleCase("this is an elephant")));
