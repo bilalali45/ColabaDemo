a5813d96b5ba22cea40b88d3d83596a37c8e841d
Zohaib Siddiqui <zohaib.siddiqui@rainsoftfn.com>
2020-07-08 15:56:11 +0500

    changes

--- Rainmaker2/src/Presentation/document-upload/src/shared/Components/AlertBox/AlertBox.tsx
+++ Rainmaker2/src/Presentation/document-upload/src/shared/Components/AlertBox/AlertBox.tsx
@@ -13,13 +13,15 @@ export const AlertBox = ({hideAlert} : AlertBoxType) => {
             <div className="alert-box--modal">
                 <button className="alert-box--modal-close" onClick={()=>{hideAlert()}} ><em className="zmdi zmdi-close"></em></button>
                 <header className="alert-box--modal-header">
-                    <h1 className="text-center">Helo world</h1>
+                    <h2 className="text-center">Are you sure, you want to proceed!</h2>
                 </header>
                 <section className="alert-box--modal-body">
                     <p>Warning! Some files are still in progess, please complete them before proceeding to the next Document</p>
                 </section>
                 <footer className="alert-box--modal-footer">
-                    <p className="text-center"><button className="btn btn-primary">OK</button></p>
+                    <p className="text-center">
+                        <button className="btn btn-primary btn-small">Yes</button> <button className="btn btn-default btn-small">No</button>
+                    </p>
                 </footer>
             </div>            
         </div>
