88ed5c47e78708e1268343fdb9c7a1bb083e7d96
Zohaib Siddiqui <zohaib.siddiqui@rainsoftfn.com>
2020-07-10 18:39:09 +0500

    Add New List

--- Rainmaker2/src/Presentation/admin/MCU/src/Components/MCUHome/NeedList/NeedListView/NeedListViewHeader/NeedListViewHeader.tsx
+++ Rainmaker2/src/Presentation/admin/MCU/src/Components/MCUHome/NeedList/NeedListView/NeedListViewHeader/NeedListViewHeader.tsx
@@ -1,18 +1,41 @@
 import React from 'react'
 
 export const NeedListViewHeader = () => {
+
+    let dropdown = {
+        //display:"block"
+    }
+
     return (
         <div className="need-list-view-header" id="NeedListViewHeader" data-component="NeedListViewHeader">
             <div className="need-list-view-header--left">
                 <span className="h2">Needs List</span> 
                 <div className="btn-group">
-                    <button className="btn btn-primary btn-sm dropdown-toggle">Add <span className="rotate-plus"></span></button>
-                    <div className="dropdown-menu">
-                        <a className="dropdown-item" href="#">Action</a>
-                        <a className="dropdown-item" href="#">Another action</a>
-                        <a className="dropdown-item" href="#">Something else here</a>
-                        <div className="dropdown-divider"></div>
-                        <a className="dropdown-item" href="#">Separated link</a>
+                    <button className="btn btn-primary btn-sm mcu-dropdown-toggle no-caret">Add <span className="btn-icon-right"><span className="rotate-plus"></span></span></button>
+                    <div className="dropdown-menu padding" style={dropdown} >
+                        <h2>Select a need list Template</h2>
+                        <h3>My Templates</h3>
+                        <ul className="checklist">
+                            <li><label><input type="checkbox" /> Income templates</label></li>
+                            <li><label><input type="checkbox" /> My standard checklist</label></li>
+                            <li><label><input type="checkbox" /> Assets template</label></li>
+                        </ul> 
+
+                        <h3>Templates by Tenants</h3>
+                        <ul className="checklist">
+                            <li><label><input type="checkbox" /> FHA Full Doc Refinance - W2</label></li>
+                            <li><label><input type="checkbox" /> VA Cash Out - W-2</label></li>
+                            <li><label><input type="checkbox" /> FHA Full Doc Refinance</label></li>
+                            <li><label><input type="checkbox" /> Conventional Refinance - SE</label></li>
+                            <li><label><input type="checkbox" /> VA Purchase - W-2</label></li>
+                            <li><label><input type="checkbox" /> Additional Questions</label></li>
+                            <li><label><input type="checkbox" /> Auto Loan</label></li>
+                            <li><label><input type="checkbox" /> Construction Loan-Phase 1</label></li>
+                        </ul> 
+
+                        <div className="external-link">
+                            <a href="">Start from new list</a>
+                        </div>
                     </div> 
                 </div>
             </div>
