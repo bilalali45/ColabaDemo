7ce1dcda2007dba7acf8cbd4e89bc3d75e5550b4
muhammad.usman <muhammad.usman@rainsoftfn.com>
2020-07-13 19:45:14 +0500

    UI Fixes

--- Rainmaker2/src/Presentation/admin/MCU/src/Components/MCUHome/TemplateManager/NewTemplate/NewTemplate.tsx
+++ Rainmaker2/src/Presentation/admin/MCU/src/Components/MCUHome/TemplateManager/NewTemplate/NewTemplate.tsx
@@ -1,9 +1,36 @@
 import React from 'react'
-
+import checkicon from '../../../../Assets/images/checkicon.svg'
+import emptyIcon from '../../../../Assets/images/empty-icon.svg'
 export const NewTemplate = () => {
     return (
-        <div>
-            <h1>NewTemplate</h1>
-        </div>
+        <section className="add-newTemp-wrap">
+            <div className="T-head">
+                <p className="editable"> <input value="My standard checklist" className="editable-TemplateTitle" /> 
+                 <span className="editsaveicon"><img src={checkicon} alt="" /></span></p>
+            </div>
+            <div className="empty-wrap">
+
+<div className="c-wrap">
+<div className="icon-wrap">
+    <img src={emptyIcon} alt="" />
+</div>
+<div className="content">
+    <p><b>Nothing</b> 
+    <br />
+    Your template is empty
+    </p>
+
+    
+    <div className="add-doc-link-wrap">
+                <a className="add-doc-link">
+                 Add Document <i className="zmdi zmdi-plus"></i>
+                </a>
+            </div>
+</div>
+</div>
+
+            </div>
+            
+        </section>
     )
 }
