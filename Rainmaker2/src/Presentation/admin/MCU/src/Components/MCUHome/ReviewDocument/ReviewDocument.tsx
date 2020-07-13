56b5caf2b720c71d0aed3bf3679997dc6a587fdc
Zohaib Siddiqui <zohaib.siddiqui@rainsoftfn.com>
2020-07-13 11:07:21 +0500

    ReviewDocument

---
+++ Rainmaker2/src/Presentation/admin/MCU/src/Components/MCUHome/ReviewDocument/ReviewDocument.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const ReviewDocument = () => {
+    return (
+        <div id="" data-component="ReviewDocument">
+
+        </div>
+    )
+}
---
+++ Rainmaker2/src/Presentation/admin/MCU/src/Components/MCUHome/ReviewDocument/ReviewDocumentHeader/ReviewDocumentHeader.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const ReviewDocumentHeader = () => {
+    return (
+        <div id="" data-component="ReviewDocumentHeader">
+
+        </div>
+    )
+}
