4d4be710f6864c3598b7f8915dea295e1b409d3c
Jehangir Babul <jehangir.babul@rainsoftfn.com>
2020-07-14 15:31:02 +0500

    error on LocalDB

--- Rainmaker2/src/Presentation/admin/MCU/src/Utils/LocalDB.ts
+++ Rainmaker2/src/Presentation/admin/MCU/src/Utils/LocalDB.ts
@@ -5,8 +5,8 @@ const cookies = new Cookies();
 export class LocalDB {
   static getCredentials() {
     const credentials = {
-      userName: LocalDB.getLoginUserName(),
-      password: LocalDB.getLoginPassword(),
+      userName: LocalDB.getLoginDevUserName(),
+      password: LocalDB.getLoginDevPassword(),
       employee: true,
     };
 
