99c4ac70a854a344e55c5634f31699b36c3bdde5
Jehangir <jehangir.babul@rainsoftfn.com>
2020-07-16 20:22:57 +0500

    added template link

--- Rainmaker2/src/Presentation/admin/MCU/src/Components/MCUHome/NeedList/NeedListHeader/NeedListHeader.tsx
+++ Rainmaker2/src/Presentation/admin/MCU/src/Components/MCUHome/NeedList/NeedListHeader/NeedListHeader.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 export const NeedListHeader = () => {
+    const history = useHistory();
+
+const redirectToTemplate= () => {
+    
+    history.push('/templateManager')
+}
+
+
     return (
         <div className="need-list-header">
             <div className="need-list-header--left">
                 <a href="#" className="btn btn-back"><em className="zmdi zmdi-arrow-left"></em> Back</a>
             </div>
             <div className="need-list-header--right">
-                <button className="btn btn-secondry"><em className="icon-record"></em> Manage Template</button> <button className="btn btn-primary" style={{pointerEvents:'none'}}><em className="icon-edit"></em> Post to Byte Pro</button>
+                <button onClick={redirectToTemplate} className="btn btn-secondry"><em className="icon-record"></em> Manage Template</button> 
+                <button className="btn btn-primary" style={{pointerEvents:'none'}}><em className="icon-edit"></em> Post to Byte Pro</button>
             </div>
         </div>
     )
