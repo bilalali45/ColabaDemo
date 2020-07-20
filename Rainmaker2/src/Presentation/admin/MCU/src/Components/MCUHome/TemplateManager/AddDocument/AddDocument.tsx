474ac187f7f588196ad818e395b8eb03ebd6f23d
Danish Iqbal <danish.iqbal@rainsoftfn.com>
2020-07-20 14:04:50 +0500

    fixed issues with template select

--- Rainmaker2/src/Presentation/admin/MCU/src/Components/MCUHome/TemplateManager/AddDocument/AddDocument.tsx
+++ Rainmaker2/src/Presentation/admin/MCU/src/Components/MCUHome/TemplateManager/AddDocument/AddDocument.tsx
@@ -10,11 +10,13 @@ import { TemplateActionsType } from '../../../../Store/reducers/TemplatesReducer
 import { Document } from '../../../../Entities/Models/Document'
 import { CategoryDocument } from '../../../../Entities/Models/CategoryDocument'
 import Overlay from 'react-bootstrap/Overlay'
+
 type AddDocumentType = {
-    popoverplacement?: any,
+    popoverplacement?: any;
+    setLoaderVisible: Function;
+
 }
-export const AddDocument = ({ popoverplacement = "bottom" }: AddDocumentType) => {
-    const [popshow, setshow] = useState(true);
+export const AddDocument = ({ popoverplacement = "bottom", setLoaderVisible }: AddDocumentType) => {
     const [show, setShow] = useState(false);
     const [target, setTarget] = useState(null);
     const ref = useRef(null);
@@ -82,15 +84,11 @@ export const AddDocument = ({ popoverplacement = "bottom" }: AddDocumentType) =>
         };
     }
 
-    const showpopover = () => {
-        setshow(!popshow)
-    }
-
     const addDocToTemplate = async (docName: string, type: string) => {
         if (!docName?.length || docName?.length > 255) {
             return;
         }
-
+        setLoaderVisible(true);
         if (templateDocuments.find((t: any) => t.docName?.toLowerCase() === docName?.toLowerCase())) {
             return;
         }
@@ -103,6 +101,7 @@ export const AddDocument = ({ popoverplacement = "bottom" }: AddDocumentType) =>
         } catch (error) {
 
         }
+        setLoaderVisible(false);
     }
 
     const renderPopOverContent = () => {
