cf6b728719cd0c75510e4c4cd360410caf596687
danishfaiz <danish.faiz@rainsoftfn.com>
2020-07-20 14:02:42 +0500

    Code Refactoring

--- Rainmaker2/src/MicroServices/Identity/Identity.API/Models/RefreshTokenRequest.cs
+++ Rainmaker2/src/MicroServices/Identity/Identity.API/Models/RefreshTokenRequest.cs
@@ -1,8 +1,13 @@
-﻿namespace Identity.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.Models
 {
     public class RefreshTokenRequest
     {
+        [RegularExpression(pattern: @"^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$",ErrorMessage = "Validation failed") ]
         public string Token { get; set; }
+
+        [RegularExpression(pattern: @"^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$", ErrorMessage = "Validation failed")]
         public string RefreshToken { get; set; }
     }
 }
