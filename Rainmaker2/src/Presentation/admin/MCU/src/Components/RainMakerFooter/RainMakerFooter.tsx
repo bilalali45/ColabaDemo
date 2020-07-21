5fcb3e71e353c77cc830fe67ec17c6c0548c7a7f
Jehangir <jehangir.babul@rainsoftfn.com>
2020-07-21 13:38:36 +0500

    Change footer text

--- Rainmaker2/src/Presentation/admin/MCU/src/Components/RainMakerFooter/RainMakerFooter.tsx
+++ Rainmaker2/src/Presentation/admin/MCU/src/Components/RainMakerFooter/RainMakerFooter.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const RainMakerFooter = () => {
     return (
         <footer className="main-footer">
-            <p>Powerd by Rainsoft 2.0</p>
+            <p>Powered by Rainsoft 2.0</p>
         </footer>
     )
 }
