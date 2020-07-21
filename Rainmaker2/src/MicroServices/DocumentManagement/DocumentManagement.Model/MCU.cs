736a2a200d1a638be224e1efb234f3db9459f769
Talhan Tayyab <talha.tayyab@rainsoftfn.com>
2020-07-21 14:49:56 +0500

    added model validation in Document.

--- Rainmaker2/src/MicroServices/DocumentManagement/DocumentManagement.Model/MCU.cs
+++ Rainmaker2/src/MicroServices/DocumentManagement/DocumentManagement.Model/MCU.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace DocumentManagement.Model
 {
     public class mcuRenameModel
     {
+        [Required(ErrorMessage = ValidationMessages.ValidationFailed)]
+        [RegularExpression(@"^[A-Fa-f\d]{24}$", ErrorMessage = ValidationMessages.ValidationFailed)]
         public string id { get; set; }
+        [Required(ErrorMessage = ValidationMessages.ValidationFailed)]
+        [RegularExpression(@"^[A-Fa-f\d]{24}$", ErrorMessage = ValidationMessages.ValidationFailed)]
         public string requestId { get; set; }
+        [Required(ErrorMessage = ValidationMessages.ValidationFailed)]
+        [RegularExpression(@"^[A-Fa-f\d]{24}$", ErrorMessage = ValidationMessages.ValidationFailed)]
         public string docId { get; set; }
+        [Required(ErrorMessage = ValidationMessages.ValidationFailed)]
+        [RegularExpression(@"^[A-Fa-f\d]{24}$", ErrorMessage = ValidationMessages.ValidationFailed)]
         public string fileId { get; set; }
+        [Required(ErrorMessage = ValidationMessages.ValidationFailed)]
         public string newName { get; set; }
     }
 }
