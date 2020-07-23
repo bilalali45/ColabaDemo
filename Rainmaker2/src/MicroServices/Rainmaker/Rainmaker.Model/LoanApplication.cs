406ba4c09c8f7c5df17da523f08319c7543b2875
javedprasla <prasla@gmail.com>
2020-07-23 15:49:58 +0500

    Email work done.

--- Rainmaker2/src/MicroServices/Rainmaker/Rainmaker.Model/LoanApplication.cs
+++ Rainmaker2/src/MicroServices/Rainmaker/Rainmaker.Model/LoanApplication.cs
@@ -9,4 +9,18 @@ namespace Rainmaker.Model
         public int loanApplicationId { get; set; }
         public bool isDraft { get; set; }
     }
+
+    public class LoanApplicationModel
+    {
+        public int? OpportunityId { get; set; }
+        public int? LoanRequestId { get; set; }
+        public int? BusinessUnitId { get; set; }
+    }
+
+    public class SendBorrowerEmailModel
+    {
+        public int loanApplicationId { get; set; }
+        public string emailBody { get; set; }
+        public int activityForId { get; set; }
+    }
 }
