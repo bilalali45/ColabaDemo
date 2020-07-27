56862f7d9f7177accb94ae73ad2482b77ab0e4e0
Zohaib Siddiqui <zohaib.siddiqui@rainsoftfn.com>
2020-07-27 20:29:03 +0500

    change

--- Rainmaker2/src/Presentation/admin/MCU/src/Components/MCUHome/NeedList/NewNeedList/NewNeedListHeader/NewNeedListHeader.tsx
+++ Rainmaker2/src/Presentation/admin/MCU/src/Components/MCUHome/NeedList/NewNeedList/NewNeedListHeader/NewNeedListHeader.tsx
@@ -4,7 +4,7 @@ import Spinner from "react-bootstrap/Spinner";
 
 export const NewNeedListHeader = () => {
     return(
-        <div className="newNeed-list-header" data-component="NewNeedListHeader" id="NewNeedListHeader">
+        <div className="new-need-list-header" data-component="NewNeedListHeader" id="NewNeedListHeader">
            New Need List Header
         </div>
     )
