5824824d081108d5727f6d999c077964e07f0528
Zohaib Siddiqui <zohaib.siddiqui@rainsoftfn.com>
2020-07-27 20:28:30 +0500

    update components files

---
+++ Rainmaker2/src/Presentation/admin/MCU/src/Components/MCUHome/NeedList/NewNeedList/NewNeedListHeader/NewNeedListHeader.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useCallback, useState } from "react";
+import { Http } from "rainsoft-js";
+import Spinner from "react-bootstrap/Spinner";
+
+export const NewNeedListHeader = () => {
+    return(
+        <div className="newNeed-list-header" data-component="NewNeedListHeader" id="NewNeedListHeader">
+           New Need List Header
+        </div>
+    )
+}
