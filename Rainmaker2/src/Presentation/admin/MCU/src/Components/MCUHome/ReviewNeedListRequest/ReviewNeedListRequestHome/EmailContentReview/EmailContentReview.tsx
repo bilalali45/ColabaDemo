a51fe6a8faaae7f276865db94f2b467b866eca40
Zohaib Siddiqui <zohaib.siddiqui@rainsoftfn.com>
2020-07-30 17:25:59 +0500

    changed

--- Rainmaker2/src/Presentation/admin/MCU/src/Components/MCUHome/ReviewNeedListRequest/ReviewNeedListRequestHome/EmailContentReview/EmailContentReview.tsx
+++ Rainmaker2/src/Presentation/admin/MCU/src/Components/MCUHome/ReviewNeedListRequest/ReviewNeedListRequestHome/EmailContentReview/EmailContentReview.tsx
@@ -1,13 +1,28 @@
 import React from 'react'
 
 export const EmailContentReview = () => {
+
+    const text = () => {
+        return (`Hi Richard Glenn Randall,
+As we discussed, I’m adding addition |
+To continue your application, we need some more information.
+
+Page 5 0f 5 Case checking account
+Financial statements
+
+Complete these items as soon as possible so we can continue reviewing your application.
+
+Thanks & Regards,
+Jonny Leo`)
+    }
+
     return (
         <div className="mcu-panel-body--content">
             <div className="mcu-panel-body padding">
                 <h2 className="h2">Review email to Richard Glenn Randall</h2>
                 <p>If you'd like, you can customize this email.</p>
 
-                <textarea name="" id="" className="form-control" rows={10}></textarea>
+                <textarea name="" id="" value={text()} className="form-control" rows={17}></textarea>
 
             </div>
 
