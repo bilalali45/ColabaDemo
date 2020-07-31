51a409c4513da004f610bc452982dfe63e650286
Jehangir <jehangir.babul@rainsoftfn.com>
2020-07-31 12:49:58 +0500

    Updated text Area

--- Rainmaker2/src/Presentation/admin/MCU/src/Shared/components/TextArea.tsx
+++ Rainmaker2/src/Presentation/admin/MCU/src/Shared/components/TextArea.tsx
@@ -4,13 +4,24 @@ type TextAreaType = {
     textAreaValue: string;
     onBlurHandler: Function;
     onChangeHandler: Function;
+    isValid: boolean;
+    errorText: string;  
 }
 
-export const TextArea = ({textAreaValue, onBlurHandler, onChangeHandler}: TextAreaType) => {
+export const TextArea = ({textAreaValue, onBlurHandler, onChangeHandler, isValid, errorText}: TextAreaType) => {
+
+   const textAreaStyle = {
+    border: isValid ?  "1px solid #f00" : ""
+   };
+   
+
     return (
-        <>
-        <textarea onBlur={(e) => onBlurHandler()} value={textAreaValue} onChange = {(e) => {onChangeHandler(e)}} name="" id="" className="form-control" rows={20}>
+        <Fragment>
+        <textarea style={textAreaStyle} onBlur={(e) => onBlurHandler()} value={textAreaValue} onChange = {(e) => {onChangeHandler(e)}} name="" id="" className="form-control" rows={20}>
         </textarea>
-        </>
+        <div>
+       <p style={{color:"red"}} >{isValid ? errorText : ''}</p>
+        </div>
+        </Fragment>
     )
 }
