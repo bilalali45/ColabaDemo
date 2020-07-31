d869d7a58695496d7ae6393ffb9d692720669bea
hammad <hammad.khalil@rainsoftfn.com>
2020-07-31 15:17:04 +0500

    Asset macro updated

--- Rainmaker2/src/MicroServices/ByteWebConnector/ByteWebConnector.API/Macros/Asset.js
+++ Rainmaker2/src/MicroServices/ByteWebConnector/ByteWebConnector.API/Macros/Asset.js
@@ -1,35 +1,35 @@
 
-if (ev.TableAndFieldName == "Asset.Name" || ev.TableAndFieldName == "Asset.AccountType1"  || ev.TableAndFieldName == "Asset.AccountNo1"|| ev.TableAndFieldName == "Asset.AccountBalance1"){
+if (ev.TableAndFieldName == "Asset.Name" || ev.TableAndFieldName == "Asset.AccountType1" || ev.TableAndFieldName == "Asset.AccountNo1" || ev.TableAndFieldName == "Asset.AccountBalance1") {
     var name = ev.Table.GetFieldValue("Name");
-    var accountType1 = ev.Table.GetField("AccountType1");
+    var accountType1 = ev.Table.GetFieldValue("AccountType1");
     var accountNo1 = ev.Table.GetFieldValue("AccountNo1");
     var accountBalance1 = ev.Table.GetFieldValue("AccountBalance1");
-    var borId = ev.Table.GetField("BorrowerID");
+    var borrowerId = ev.Table.GetFieldValue("BorrowerID");
     var fileDataId = ev.Table.GetFieldValue("FileDataID");
- 
- var useProxy = true;
- 
- 
- 
-     var dataRaw = " --data-raw \"{ \"\"name\"\":\"\"{{name}}\"\" ,\"\"accountType1\"\":\"{{accountType1}}\" , \"\"accountNo1\"\":\"\"{{accountNo1}}\"\" , \"\"accountBalance1\"\":\"\"{{accountBalance1}}\"\" , \"\"borrowerID\"\":\"{{borId}}\" , \"\"fileDataId\"\":\"{{fileDataId}}\"  }\" ";
- 
-     var arguments = "{{proxy}} --location --request POST \"http://localhost:52537/api/Values/asset\" --header \"Content-Type: application/json\" --header \"Accept: application/json\" {{dataRaw}}";
- 
-     if (useProxy) {
-         arguments = arguments.replace("{{proxy}}", "--proxy 127.0.0.1:8888");
-     }
-     dataRaw = dataRaw.replace("{{name}}", name);
-     dataRaw = dataRaw.replace("{{accountType1}}", accountType1);
-     dataRaw = dataRaw.replace("{{accountNo1}}", accountNo1);
-     dataRaw = dataRaw.replace("{{accountBalance1}}", accountBalance1);
-     dataRaw = dataRaw.replace("{{borId}}", borId);
-     dataRaw = dataRaw.replace("{{fileDataId}}", fileDataId);
- 
-     arguments = arguments.replace("{{dataRaw}}", dataRaw);
-     var process = new System.Diagnostics.Process();
-     process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-     process.StartInfo.CreateNoWindow = true;
-     process.StartInfo.FileName = "curl.exe";
-     process.StartInfo.Arguments = arguments;
-     process.Start();
- }
+
+    var useProxy = true;
+
+
+
+    var dataRaw = " --data-raw \"{ \"\"name\"\":\"\"{{name}}\"\" ,\"\"accountType1\"\":\"\"{{accountType1}}\"\" , \"\"accountNo1\"\":\"\"{{accountNo1}}\"\" , \"\"accountBalance1\"\":{{accountBalance1}} , \"\"borrowerID\"\":{{borrowerId}} , \"\"fileDataId\"\":{{fileDataId}} } ";
+
+    var arguments = "{{proxy}} --location --request POST \"http://localhost:5050/api/ByteWebConnector/BorrowerAsset/update\" --header \"Content-Type: application/json\" --header \"Accept: application/json\" {{dataRaw}}";
+
+    if (useProxy) {
+        arguments = arguments.replace("{{proxy}}", "--proxy 127.0.0.1:8888");
+    }
+    dataRaw = dataRaw.replace("{{name}}", name);
+    dataRaw = dataRaw.replace("{{accountType1}}", accountType1);
+    dataRaw = dataRaw.replace("{{accountNo1}}", accountNo1);
+    dataRaw = dataRaw.replace("{{accountBalance1}}", (accountBalance1) ? accountBalance1 : null);
+    dataRaw = dataRaw.replace("{{borrowerId}}", (borrowerId) ? borrowerId : null);
+    dataRaw = dataRaw.replace("{{fileDataId}}", fileDataId);
+
+    arguments = arguments.replace("{{dataRaw}}", dataRaw);
+    var process = new System.Diagnostics.Process();
+    process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+    process.StartInfo.CreateNoWindow = true;
+    process.StartInfo.FileName = "curl.exe";
+    process.StartInfo.Arguments = arguments;
+    process.Start();
+}
--- Rainmaker2/src/MicroServices/ByteWebConnector/ByteWebConnector.API/Models/ByteAsset.cs
+++ Rainmaker2/src/MicroServices/ByteWebConnector/ByteWebConnector.API/Models/ByteAsset.cs
@@ -3,8 +3,8 @@
     public class ByteAsset
     {
         public int AppNo { get; set; }
-        public int AssetID { get; set; }
-        public int? BorrowerID { get; set; }
+        public int AssetId { get; set; }
+        public int? BorrowerId { get; set; }
         public int DisplayOrder { get; set; }
         public string Name { get; set; }
         public string Attn { get; set; }
@@ -14,23 +14,23 @@
         public string City { get; set; }
         public string State { get; set; }
         public string Zip { get; set; }
-        public int AccountType1 { get; set; }
+        public string AccountType1 { get; set; }
         public string AccountNo1 { get; set; }
-        public string AccountBalance1 { get; set; }
-        public int AccountType2 { get; set; }
+        public double? AccountBalance1 { get; set; }
+        public string AccountType2 { get; set; }
         public string AccountNo2 { get; set; }
-        public object AccountBalance2 { get; set; }
-        public int AccountType3 { get; set; }
+        public double? AccountBalance2 { get; set; }
+        public string AccountType3 { get; set; }
         public string AccountNo3 { get; set; }
-        public object AccountBalance3 { get; set; }
-        public int AccountType4 { get; set; }
+        public double? AccountBalance3 { get; set; }
+        public string AccountType4 { get; set; }
         public string AccountNo4 { get; set; }
-        public object AccountBalance4 { get; set; }
+        public double? AccountBalance4 { get; set; }
         public string Notes { get; set; }
         public string Fax { get; set; }
         public string AccountOtherDesc { get; set; }
-        public int AccountHeldByType { get; set; }
-        public long FileDataID { get; set; }
+        public string AccountHeldByType { get; set; }
+        public long FileDataId { get; set; }
 
 
         public object GetRainmakerBorrowerAsset()
