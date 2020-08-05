d907fc42102931f4f4738fb883c13ae4bbe1d9a1
Shehroz Reaz <shehroz.reaz@rainsoftfn.com>
2020-08-05 10:51:21 -0500

    Refratoring

---
+++ Rainmaker2/src/MicroServices/ByteWebConnector/ByteWebConnector.API/Enums/Residency.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace ByteWebConnector.API.Enums
+{
+    public enum ResidencyStateEnum
+    {
+        [Description("US Citizen")]
+        UsCitizen = 1,
+        [Description("Permanent Resident")]
+        PermanentResident = 2,
+        [Description("Valid work VISA (H1, L1 etc.)")]
+        ValidworkVisa = 3,
+        [Description("Temporary workers (H -2A)")]
+        Temporaryworkers = 4,
+        [Description("Other")]
+        Other = 5
+
+    }
+}
