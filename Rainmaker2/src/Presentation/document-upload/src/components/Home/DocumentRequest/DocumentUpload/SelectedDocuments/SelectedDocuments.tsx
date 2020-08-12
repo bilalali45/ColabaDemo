b3a3081c86958b0eadb9904100bebe94553cc220
Danish Iqbal <danish.iqbal@rainsoftfn.com>
2020-08-12 18:13:55 +0500

    fixed issue with file upload function

--- Rainmaker2/src/Presentation/document-upload/src/components/Home/DocumentRequest/DocumentUpload/SelectedDocuments/SelectedDocuments.tsx
+++ Rainmaker2/src/Presentation/document-upload/src/components/Home/DocumentRequest/DocumentUpload/SelectedDocuments/SelectedDocuments.tsx
@@ -112,10 +112,15 @@ export const SelectedDocuments = ({
 
   const uploadFiles = async () => {
     setSubBtnPressed(true);
-
+    let counter = 0;
     for (const file of selectedFiles) {
       if (file.file && file.uploadStatus !== "done" && !file.notAllowed) {
+        counter++;
         try {
+          if (counter === 1) {
+            throw new Error("file could not be uploaded");
+
+          }
           await DocumentUploadActions.submitDocuments(
             currentSelected,
             file,
@@ -123,6 +128,7 @@ export const SelectedDocuments = ({
             Auth.getLoanAppliationId()
           );
         } catch (error) {
+          file.uploadStatus = 'failed';
           console.log('error during file submit', error);
           console.log('error during file submit', error.response);
         }
@@ -130,6 +136,7 @@ export const SelectedDocuments = ({
       }
     }
     setSubBtnPressed(false);
+    console.log(selectedFiles);
     try {
       fetchUploadedDocuments();
       let docs = await DocumentActions.getPendingDocuments(
@@ -158,7 +165,7 @@ export const SelectedDocuments = ({
         }
         return p;
       });
-      console.log(updatedPendingDocs);
+      current.files = selectedFiles.filter((f: Document) => f.uploadStatus !== 'failed');
       dispatch({ type: DocumentsActionType.FetchPendingDocs, payload: updatedPendingDocs });
       dispatch({ type: DocumentsActionType.SetCurrentDoc, payload: current });
     } catch (error) {
