4ba37cee1c6cbfc045b622674fbd661503f6b2a1
Muhammad Mubashir Khan <mubashir.khan@rainsoftfn.com>
2020-08-13 11:46:07 +0500

    added notification api

---
+++ Rainmaker2/src/MicroServices/Notification/Notification.API/CorrelationHandlersAndMiddleware/ExceptionMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Notification.API.CorrelationHandlersAndMiddleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext,ILogger<ExceptionMiddleware> _logger)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex,$"Something went wrong");
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            return context.Response.WriteAsync(new
+            {
+                Code = context.Response.StatusCode.ToString(),
+                Message = "Internal Server Error"
+            }.ToString());
+        }
+    }
+}
