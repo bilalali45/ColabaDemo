0af74674f13d7452f4556547177344f083870d46
faisal <muhammad.faisal@rainsoftfn.com>
2020-08-17 11:20:09 +0500

    node sass and more structure

--- Rainmaker2/src/Presentation/notifications/src/pages/HomePage.tsx
+++ Rainmaker2/src/Presentation/notifications/src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import {Notifications} from '../features/Notifications';
+import {Header} from './_HomePage';
 
 export const HomePage = () => {
   return (
     <div>
+      <Header />
       <Notifications />
     </div>
   );
