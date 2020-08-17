08ee31dfb6f003f24127d23cda1437e3dd489297
faisal <muhammad.faisal@rainsoftfn.com>
2020-08-17 10:25:25 +0500

    basic app structure

---
+++ Rainmaker2/src/Presentation/notifications/src/app/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { HomePage } from "../pages/HomePage";
+import { AppRoutes } from "./routes";
+
+function App() {
+  return <AppRoutes />;
+}
+
+export default App;
---
+++ Rainmaker2/src/Presentation/notifications/src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Notifications } from "../features/Notifications";
+
+export const HomePage = () => {
+  return (
+    <div>
+      <Notifications />
+    </div>
+  );
+};
