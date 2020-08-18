cb3cafe3800661fcfda9b8044a2fe1b34bcc6e68
faisal <muhammad.faisal@rainsoftfn.com>
2020-08-18 10:38:37 +0500

    eslint setup

---
+++ Rainmaker2/src/Presentation/notifications/.eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  settings: {
+    react: {
+      createClass: 'createReactClass',
+      pragma: 'React',
+      version: 'detect'
+    }
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'prettier/react'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', '@typescript-eslint', 'react-hooks', 'unused-imports'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    'unused-imports/no-unused-imports-ts': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'error',
+    'unused-imports/no-unused-vars-ts': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_'
+      }
+    ],
+    'react-hooks/exhaustive-deps': 'error',
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      {functions: true, classes: true, variables: false}
+    ]
+  }
+};
