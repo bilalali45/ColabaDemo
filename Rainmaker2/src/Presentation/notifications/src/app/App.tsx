097c73acd2c232ffe5dfe64214c0b35e956feff6
Jehangir <jehangir.babul@rainsoftfn.com>
2020-08-19 16:10:42 +0500

    Implemented SignalR

--- Rainmaker2/src/Presentation/notifications/src/app/App.tsx
+++ Rainmaker2/src/Presentation/notifications/src/app/App.tsx
@@ -1,8 +1,10 @@
-import {AppRoutes} from './routes';
+import React, {FunctionComponent} from 'react';
 import {useEffect, useState} from 'react';
+import {SignalRHub} from 'rainsoft-js';
+
 import {LocalDB} from '../Utils/LocalDB';
 import {UserActions} from '../actions/UserActions';
-import React from 'react';
+import {AppRoutes} from './routes';
 
 declare global {
   interface Window {
@@ -11,10 +13,25 @@ declare global {
 }
 window.envConfig = window.envConfig || {};
 
-export const App = () => {
+export const App: FunctionComponent = () => {
   const [authenticated, setAuthenticated] = useState<boolean>(false);
+
   useEffect(() => {
     console.log('MCU App Version', '0.0.1');
+    const authenticate = async () => {
+      console.log('Before Authorize');
+      const isAuth = await UserActions.authorize();
+      setAuthenticated(Boolean(isAuth));
+      console.log('After Authorize');
+      const accessToken: string = LocalDB.getAuthToken() || '';
+      SignalRHub.configureHubConnection(
+        window.envConfig.API_BASE_URL + '/serverhub',
+        accessToken,
+        eventsRegister
+      );
+      UserActions.addExpiryListener();
+    };
+
     authenticate();
     // component unmount
     return () => {
@@ -22,12 +39,22 @@ export const App = () => {
     };
   }, []);
 
-  const authenticate = async () => {
-    console.log('Before Authorize');
-    let isAuth = await UserActions.authorize();
-    setAuthenticated(Boolean(isAuth));
-    console.log('After Authorize');
-    UserActions.addExpiryListener();
+  const eventsRegister = () => {
+    console.log('signalR eventsRegister on Client', SignalRHub.hubConnection);
+    SignalRHub.hubConnection.on('TestSignalR', (data: string) => {
+      console.log(`TestSignalR`, data);
+    });
+    SignalRHub.hubConnection.on('SendNotification', (data: any) => {
+      console.log('Notification comes from SignalR on Client', data);
+    });
+
+    SignalRHub.hubConnection.onclose((e: any) => {
+      console.log(`SignalR disconnected on Client`, e);
+      const auth = LocalDB.getAuthToken();
+      if (auth) {
+        SignalRHub.signalRHubResume();
+      }
+    });
   };
 
   console.log('Authorize User is authenticated', authenticated);
