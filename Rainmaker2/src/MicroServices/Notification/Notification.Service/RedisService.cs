796b809f98aaaeb87e7625b02e1c545bc00de8a0
Muhammad Mubashir Khan <mubashir.khan@rainsoftfn.com>
2020-08-20 19:02:16 +0500

    added redis service

---
+++ Rainmaker2/src/MicroServices/Notification/Notification.Service/RedisService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Notification.Service
+{
+    public class RedisService : IRedisService
+    {
+        private readonly IServiceProvider serviceProvider;
+
+        public RedisService(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+        public void Run()
+        {
+            using (IServiceScope scope = serviceProvider.CreateScope())
+            {
+                while (true)
+                {
+                    INotificationService notificationService =
+                        scope.ServiceProvider.GetRequiredService<INotificationService>();
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
