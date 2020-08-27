2e840b0a3cc04301ac7bfa75c2cf3810d066a5a1
Muhammad Usman <iam.usmansarwar@gmailo.com>
2020-08-27 16:20:57 +0500

    Rain-969 (Sync to LOS error Modal)

--- Rainmaker2/src/Presentation/admin/MCU/src/Components/MCUHome/NeedList/NeedListView/NeedListAlertBox/NeedListAlertBox.tsx
+++ Rainmaker2/src/Presentation/admin/MCU/src/Components/MCUHome/NeedList/NeedListView/NeedListAlertBox/NeedListAlertBox.tsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from "react-bootstrap/Button";
 import { NeedList } from '../../../../../Entities/Models/NeedList';
-
+import errorIcon from '../../../../../Assets/images/error-icon.svg';
 type NeedListAlertProps = {
     showFailedToSyncBox: boolean;
     needList: NeedList;
@@ -12,21 +12,80 @@ export const NeedListAlertBox = ({showFailedToSyncBox, needList} : NeedListAlert
     return (
         <Modal
         show={showFailedToSyncBox}
+        //show={true} 
         // onHide={handleClose}
         backdrop="static"
         keyboard={true}
-        className="modal-alert"
+        className="modal-alert sync-error-alert"
+
         centered
     >
         <Modal.Header closeButton>
-        Document Synchronization Failed
+            <div className="h-wrap">
+                <div className="e-icon">
+                    <img src={errorIcon} alt="" />
+                </div>
+                <div className="e-content">
+                    <h4>Document Synchronization Failed</h4>
+                    <p>Synchronization was not successfully completed</p>
+                </div>
+            </div>
+        
         </Modal.Header>
 
         <Modal.Body>
-        
-            <p className="text-center">
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae dignissimos ducimus aspernatur earum beatae mollitia ex nisi nobis soluta maiores dicta quae deserunt, ut non tenetur accusantium veritatis atque perferendis.
-            </p>
+  
+  <div className="sync-modal-alert-body">
+
+      <h4>Documents that have failed to sync</h4>
+      <ul>
+          <li>
+              <h5>Bank Statement</h5>
+              <p>Bank-statement-Jan-to-Mar-2020-1.jpg</p>
+              <p>Bank-statement-Jan-to-Mar-2020-1.jpg</p>
+              <p>Bank-statement-Jan-to-Mar-2020-1.jpg</p>
+          </li>
+
+          <li>
+              <h5>Personal Tax Return</h5>
+              <p>Personal Tax Returns.pdf</p>
+          </li>
+
+          <li>
+              <h5>Alimony Income Verification</h5>
+              <p>Income Verification.pdf</p>
+          </li>
+
+          <li>
+              <h5>Alimony Income Verification</h5>
+              <p>Income Verification.pdf</p>
+          </li>
+
+          <li>
+              <h5>Alimony Income Verification</h5>
+              <p>Income Verification.pdf</p>
+          </li>
+
+
+          <li>
+              <h5>Alimony Income Verification</h5>
+              <p>Income Verification.pdf</p>
+          </li>
+
+
+          <li>
+              <h5>Alimony Income Verification</h5>
+              <p>Income Verification.pdf</p>
+          </li>
+
+
+          <li>
+              <h5>Alimony Income Verification</h5>
+              <p>Income Verification.pdf</p>
+          </li>
+      </ul>
+
+  </div>
         </Modal.Body>
         <Modal.Footer>
         <p className="text-center">
