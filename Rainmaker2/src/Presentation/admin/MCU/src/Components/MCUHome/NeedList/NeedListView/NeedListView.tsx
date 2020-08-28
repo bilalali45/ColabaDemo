1bbe058c6b2b817b4654cb63a0cb38bc72315a3a
Jehangir <jehangir.babul@rainsoftfn.com>
2020-08-28 19:36:33 +0500

    Set auto and manual

--- Rainmaker2/src/Presentation/admin/MCU/src/Components/MCUHome/NeedList/NeedListView/NeedListView.tsx
+++ Rainmaker2/src/Presentation/admin/MCU/src/Components/MCUHome/NeedList/NeedListView/NeedListView.tsx
@@ -302,7 +302,7 @@ export const NeedListView = () => {
     let res: any = await NeedListActions.checkIsByteProAuto();
     console.log('checkIsByteProAuto', res.syncToBytePro)
     let isAuto = res.syncToBytePro != 2 ? true : false;
-    dispatch({type: NeedListActionsType.SetIsByteProAuto, payload: false})
+    dispatch({type: NeedListActionsType.SetIsByteProAuto, payload: isAuto})
   }
 
   const deleteNeedListDoc = async (
