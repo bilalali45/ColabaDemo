436b94316727bfb159eb4178732763b5fcdb7c0c
Khalid Halim <khalid.halim@rainsoftfn.com>
2020-09-02 16:20:57 +0500

    Authentication fixed

--- Rainmaker2/src/Presentation/document-upload/src/store/actions/User.ts
+++ Rainmaker2/src/Presentation/document-upload/src/store/actions/User.ts
@@ -1,17 +1,18 @@
-import { Http } from 'rainsoft-js';
+import { Http } from "rainsoft-js";
 import { Auth } from "../../services/auth/Auth";
 
-const httpClient = new Http()
+const httpClient = new Http();
 
 export class AuthActions {
-
-    static async login(payload: any) {
-        const res: any = await httpClient.post('/login', { email: 'test@test.com', password: 'test123' });
-        let token = res?.data?.token;
-        if (token) {
-            Auth.saveAuth(token);
-            return token;
-        }
+  static async login(payload: any) {
+    const res: any = await httpClient.post("/login", {
+      email: Auth.getLoginUserName(),
+      password: Auth.getLoginPassword(),
+    });
+    let token = res?.data?.token;
+    if (token) {
+      Auth.saveAuth(token);
+      return token;
     }
+  }
 }
-
