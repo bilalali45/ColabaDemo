325068cd2096fb934f20c7387a2f28c05d40653d
Shehroz Reaz <shehroz.reaz@rainsoftfn.com>
2020-09-03 05:00:01 -0500

    call fixed

--- Rainmaker2/src/Library/ServiceCallHelper/Call.cs
+++ Rainmaker2/src/Library/ServiceCallHelper/Call.cs
@@ -33,7 +33,7 @@ namespace ServiceCallHelper
                                              string endPoint,
                                              string bearerToken = null)
         {
-            if (!bearerToken.HasValue())
+            if (bearerToken.HasValue())
                 httpClient.DefaultRequestHeaders.Authorization
                     = new AuthenticationHeaderValue(scheme: "Bearer",
                                                     parameter: bearerToken);
@@ -70,7 +70,8 @@ namespace ServiceCallHelper
 
             return Post<T>(httpClient: httpClient,
                            endPoint: endPoint,
-                           content: token);
+                           content: content,
+                           bearerToken: token);
         }
 
 
@@ -79,7 +80,7 @@ namespace ServiceCallHelper
                                               string content,
                                               string bearerToken = null)
         {
-            if (!bearerToken.HasValue())
+            if (bearerToken.HasValue())
                 httpClient.DefaultRequestHeaders.Authorization
                     = new AuthenticationHeaderValue(scheme: "Bearer",
                                                     parameter: bearerToken);
