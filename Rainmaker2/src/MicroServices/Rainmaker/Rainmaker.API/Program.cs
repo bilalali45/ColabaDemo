a8f83ce93d42a1fa6e60ad6dac29efba9b2e401c
danishfaiz <danish.faiz@rainsoftfn.com>
2020-09-10 13:30:44 +0500

    changes in file loggers

--- Rainmaker2/src/MicroServices/Rainmaker/Rainmaker.API/Program.cs
+++ Rainmaker2/src/MicroServices/Rainmaker/Rainmaker.API/Program.cs
@@ -1,13 +1,13 @@
+using System;
+using System.Net;
+using System.Reflection;
+using System.Threading;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Exceptions;
 using Serilog.Sinks.Elasticsearch;
-using System;
-using System.Net;
-using System.Reflection;
-using System.Threading;
 
 namespace Rainmaker.API
 {
@@ -18,8 +18,10 @@ namespace Rainmaker.API
             Environment.CurrentDirectory = AppContext.BaseDirectory;
             ServicePointManager.DefaultConnectionLimit = int.MaxValue;
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls13;
-            ThreadPool.SetMaxThreads(1000, 1000);
-            ThreadPool.SetMinThreads(1000,1000);
+            ThreadPool.SetMaxThreads(workerThreads: 1000,
+                                     completionPortThreads: 1000);
+            ThreadPool.SetMinThreads(workerThreads: 1000,
+                                     completionPortThreads: 1000);
             ConfigureLogging();
             CreateHost(args: args);
         }
@@ -45,7 +47,10 @@ namespace Rainmaker.API
                          //.WriteTo.Console()
                          .WriteTo.Async(configure: x => x.File(path: $"Logs\\{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(oldValue: ".", newValue: "-")}-serviceLog-.log",
                                                                retainedFileCountLimit: 7,
-                                                               outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{CorrelationId}] [{Level}] {Message}{NewLine}{Exception}", rollingInterval: RollingInterval.Day)
+                                                               rollOnFileSizeLimit: true,
+                                                               fileSizeLimitBytes: 256 * 1024 * 1024,
+                                                               outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{CorrelationId}] [{Level}] {Message}{NewLine}{Exception}",
+                                                               rollingInterval: RollingInterval.Day)
                                        )
                          .WriteTo.Elasticsearch(options: ConfigureElasticSink(configuration: configuration,
                                                                               environment: environment))
@@ -60,10 +65,10 @@ namespace Rainmaker.API
                                                                      string environment)
         {
             return new ElasticsearchSinkOptions(node: new Uri(uriString: configuration[key: "ElasticConfiguration:Uri"]))
-            {
-                AutoRegisterTemplate = true,
-                IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(oldValue: ".", newValue: "-")}-{environment?.ToLower().Replace(oldValue: ".", newValue: "-")}-{DateTime.UtcNow:yyyy-MM}"
-            };
+                   {
+                       AutoRegisterTemplate = true,
+                       IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(oldValue: ".", newValue: "-")}-{environment?.ToLower().Replace(oldValue: ".", newValue: "-")}-{DateTime.UtcNow:yyyy-MM}"
+                   };
         }
 
 
@@ -85,7 +90,7 @@ namespace Rainmaker.API
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args: args)
-                .UseWindowsService()
+                       .UseWindowsService()
                        .ConfigureWebHostDefaults(configure: webBuilder => { webBuilder.UseStartup<Startup>(); })
                        .ConfigureAppConfiguration(configureDelegate: configuration =>
                        {
