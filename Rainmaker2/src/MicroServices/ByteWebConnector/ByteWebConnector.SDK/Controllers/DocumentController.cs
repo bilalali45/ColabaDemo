4b1bbdb32ea85d2c91fafdab187ff5fa26dc5616
hammad <hammad.khalil@rainsoftfn.com>
2020-09-15 21:45:08 +0500

    log added in BWC SDK

--- Rainmaker2/src/MicroServices/ByteWebConnector/ByteWebConnector.SDK/Controllers/DocumentController.cs
+++ Rainmaker2/src/MicroServices/ByteWebConnector/ByteWebConnector.SDK/Controllers/DocumentController.cs
@@ -9,7 +9,9 @@ using ByteWebConnector.SDK.Models;
 using ByteWebConnector.SDK.Models.ControllerModels.Document.Response;
 using LOSAutomation;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Serilog;
 using Xceed.Pdf;
 
 namespace ByteWebConnector.SDK.Controllers
@@ -18,6 +20,13 @@ namespace ByteWebConnector.SDK.Controllers
     [ApiController]
     public class DocumentController : ControllerBase
     {
+        private readonly ILogger<DocumentController> _logger;
+
+
+        public DocumentController(ILogger<DocumentController> logger)
+        {
+            _logger = logger;
+        }
 
         public IEnumerable<string> Get()
         {
@@ -38,6 +47,7 @@ namespace ByteWebConnector.SDK.Controllers
         {
             try
             {
+                _logger.LogInformation("====DocsyncSDK SendSdkDocument Start====");
                 var byteProSettings = sdkSendDocumentRequest.ByteProSettings;
                 var documentUploadRequest = sdkSendDocumentRequest.DocumentUploadRequest;
 
@@ -46,34 +56,38 @@ namespace ByteWebConnector.SDK.Controllers
                 SDKSession session = _sessionStatic;
                 if (session != null)
                 {
+                    _logger.LogInformation("====DocsyncSDK old session found====");
                     try
                     {
                         application = session.GetApplication();
                     }
                     catch (Exception e)
                     {
+                        _logger.LogInformation($"DocsyncSDK Session Exception={e.Message}");
                         session = null;
                     }
                 }
-
+                
                 if (application == null)
                 {
+                    _logger.LogInformation($"DocsyncSDK application= null");
                     session = new SDKSession();
-                    string sessionDataFolder = session.DataFolder;
                     session.Login(byteProSettings.ByteUserName, byteProSettings.BytePassword, byteProSettings.ByteConnectionName);
                     session.Authorize(byteProSettings.ByteCompanyCode, byteProSettings.ByteUserNo, byteProSettings.ByteAuthKey);
+                    _logger.LogInformation($"DocsyncSDK ===Authorized===");
                     _sessionStatic = session;
                     application = session.GetApplication();
                 }
 
-
+                _logger.LogInformation($"DocsyncSDK application found");
 
                 SDKFile file = application.OpenFile(documentUploadRequest.FileName, false);
-
+                _logger.LogInformation($"DocsyncSDK File Opened");
                 int index = file.AddCollectionObject("EmbeddedDoc");
                 SDKEmbeddedDoc embeddedDoc = file.GetCollectionObject("EmbeddedDoc", index) as SDKEmbeddedDoc;
                 if (embeddedDoc != null)
                 {
+                    _logger.LogInformation($"DocsyncSDK EmbeddedDoc Added");
                     string fileNameAndPath = Path.GetTempPath() + documentUploadRequest.DocumentName + "." +
                                                documentUploadRequest.DocumentExension;
                     embeddedDoc.SetFieldValue("FileExtension",
@@ -86,30 +100,38 @@ namespace ByteWebConnector.SDK.Controllers
                                               documentUploadRequest.DocumentStatus);
                     embeddedDoc.SetFieldValue("Description",
                                               documentUploadRequest.DocumentName);
+                    _logger.LogInformation($"DocsyncSDK All Fields Set in Embedded Doc");
                     System.IO.File.WriteAllBytes(fileNameAndPath,
                                                  Convert.FromBase64String(documentUploadRequest.DocumentData));
+                    _logger.LogInformation($"DocsyncSDK File Write location = {fileNameAndPath}");
                     //MemoryStream stream = GenerateStreamFromString(documentUploadRequest.DocumentData);
 
                     embeddedDoc.LoadFromFile(fileNameAndPath);
+                    _logger.LogInformation($"DocsyncSDK LoadFromFile = true");
                 }
 
                 file.Save();
+                _logger.LogInformation($"DocsyncSDK LoadApplication File saved");
                 object documentId = null;
                 SDKEmbeddedDoc sdkEmbeddedDoc = file.GetCollectionObject("EmbeddedDoc", index) as SDKEmbeddedDoc;
                 if (sdkEmbeddedDoc != null)
                 {
-                   documentId = sdkEmbeddedDoc.GetField("EmbeddedDocID");
+                    _logger.LogInformation($"DocsyncSDK Getting Embedded Doc");
+                    documentId = sdkEmbeddedDoc.GetField("EmbeddedDocID");
+                   _logger.LogInformation($"DocsyncSDK DocumentId= {documentId}");
                 }
                 EmbedDocumentResponse documentResponse = new EmbedDocumentResponse()
                 {
                     DocumentId = Convert.ToInt64(documentId),
                     ExtOriginatorId = 1
                 };
+                _logger.LogInformation($"DocsyncSDK EmbedDocumentResponse= {documentResponse.DocumentId}");
+                _logger.LogInformation($"DocsyncSDK EmbedDocumentResponse= {documentResponse.ExtOriginatorId}");
                 return new SendSdkDocumentResponse() { Status = SendSdkDocumentResponse.SdkDocumentResponseStatus.Success,Data = JsonConvert.SerializeObject(documentResponse) };
             }
             catch (Exception e)
             {
-
+                _logger.LogInformation($"DocsyncSDK Exception= {e.Message}");
                 return new SendSdkDocumentResponse() { Status = SendSdkDocumentResponse.SdkDocumentResponseStatus.Error };
             }
 
