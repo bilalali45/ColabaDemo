4ce12d5e13bdaff8adc4172c37722d9528a066a8
Noor ul Huda <noor.huda@rainsoftfn.com>
2020-09-23 12:20:41 +0500

    loanSnapShot test cases

--- Rainmaker2/src/Presentation/admin/MCU/src/Store/actions/__mocks__/NeedListActions.ts
+++ Rainmaker2/src/Presentation/admin/MCU/src/Store/actions/__mocks__/NeedListActions.ts
@@ -1,9 +1,33 @@
 import {LoanApplication} from '../../../Entities/Models/LoanApplication';
 
+
+
+const LoanInfo = {
+  "borrowers": ["Taruf Ali","Co Borr Last Name"],
+  "cityName": "Houston",
+  "countryName": "",
+  "countyName": " Harris County",
+  "expectedClosingDate": "2020-08-29T00:00:00",
+  "expirationDate": "",
+  "loanAmount": 45000,
+  "loanNumber": "50020000155",
+  "loanProgram": "",
+  "loanPurpose": "Purchase a home",
+  "lockDate": "2020-08-18T05:44:40.357Z",
+  "lockStatus": "Float",
+  "popertyValue": 55000,
+  "propertyType": "Single Family Detached",
+  "rate": "",
+  "stateName": "TX",
+  "status": "Application Submitted",
+  "streetAddress": "New27AUG",
+  "unitNumber": "2708",
+  "zipCode": "77023",
+}
 export class NeedListActions {
   static async getLoanApplicationDetail(loanApplicationId: string) {
     try {
-      return new LoanApplication();
+      return LoanInfo;
     } catch (error) {
       console.log(error);
     }
