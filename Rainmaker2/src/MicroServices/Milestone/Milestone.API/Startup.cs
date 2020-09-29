ddcab36a26e598758253156dfe518bd44f15cb90
Muhammad Mubashir Khan <mubashir.khan@rainsoftfn.com>
2020-09-29 10:33:30 +0500

    removed signalr hook

--- Rainmaker2/src/MicroServices/Milestone/Milestone.API/Startup.cs
+++ Rainmaker2/src/MicroServices/Milestone/Milestone.API/Startup.cs
@@ -58,23 +58,6 @@ namespace Milestone.API
                             ValidAudience = "readers",
                             IssuerSigningKey = symmetricSecurityKey
                         };
-                        options.Events = new JwtBearerEvents
-                        {
-                            OnMessageReceived = context =>
-                            {
-                                var accessToken = context.Request.Query["access_token"];
-
-                                // If the request is for our hub...
-                                var path = context.HttpContext.Request.Path;
-                                if (!string.IsNullOrEmpty(accessToken) &&
-                                    (path.StartsWithSegments("/serverhub")))
-                                {
-                                    // Read the token out of the query string
-                                    context.Token = accessToken;
-                                }
-                                return Task.CompletedTask;
-                            }
-                        };
                     });
 
             #region HttpClient Dependency with correlation
