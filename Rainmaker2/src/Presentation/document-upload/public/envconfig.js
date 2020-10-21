9e811040e2472d785d02fb8f51ecd3878c9469e0
muhammad.usman <muhammad.usman@rainsoftfn.com>
2020-10-21 12:26:45 +0500

    env

--- Rainmaker2/src/Presentation/document-upload/public/envconfig.js
+++ Rainmaker2/src/Presentation/document-upload/public/envconfig.js
@@ -1,4 +1,5 @@
 window.envConfig = {
-  API_BASE_URL: "https://qamaingateway.rainsoftfn.com",
+ // API_BASE_URL: "https://qamaingateway.rainsoftfn.com",
+ API_BASE_URL: "https://localhost:5001",
   IDLE_TIMER: "10", // Must be in minutes
 };
