e9f46cca7beb55514b2e71dac81f1fc0d00fd2a3
Muhammad Mubashir Khan <mubashir.khan@rainsoftfn.com>
2020-11-04 13:47:01 +0500

    message updated

--- Rainmaker2/src/MicroServices/DocumentManagement/DocumentManagement.API/Controllers/FileController.cs
+++ Rainmaker2/src/MicroServices/DocumentManagement/DocumentManagement.API/Controllers/FileController.cs
@@ -99,7 +99,7 @@ namespace DocumentManagement.API.Controllers
                     return BadRequest("File type is not supported. Allowed types: PDF, JPEG, PNG");
                 if (file.Length > setting.maxFileSize)
                     //throw new DocumentManagementException("File size exceeded limit");
-                    return BadRequest($"File size over {((decimal)setting.maxFileSize)/(1024*1024)}mb limit");
+                    return BadRequest($"File size must be under {((decimal)setting.maxFileSize)/(1024*1024)} mb");
                 if (file.FileName.Length > setting.maxFileNameSize)
                     return BadRequest("File Name size exceeded limit");
                 //throw new DocumentManagementException("File Name size exceeded limit");
