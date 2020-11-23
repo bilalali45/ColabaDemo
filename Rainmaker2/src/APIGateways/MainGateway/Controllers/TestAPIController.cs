09ea22da9f08ab4b51e0877d21efebe86198c0a1
Talhan Tayyab <talha.tayyab@rainsoftfn.com>
2020-11-23 08:07:01 +0000

    Merged PR 109: added TestAPIController in Maingateway.
    added TestAPIController in Maingateway.

---
+++ Rainmaker2/src/APIGateways/MainGateway/Controllers/TestAPIController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MainGateway.Controllers
+{
+    [ApiController]
+    [Route("api/MainGateway/[controller]")]
+    public class TestAPIController : Controller
+    {
+        [HttpGet("[action]")]
+        public string TestGet(string text)
+        {
+            return "Get : " + text;
+        }
+        [HttpPost("[action]")]
+        public string TestPost(string text)
+        {
+            return "Post : " + text;
+        }
+        [HttpPut("[action]")]
+        public string TestPut(string text)
+        {
+            return "Put : " + text;
+        }
+    }
+}
