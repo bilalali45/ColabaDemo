7426bb6ec99bc6397bde6e3d879631d09dcabe4f
Azim Khan <azim.khan@rainsoftfn.com>
2020-12-18 12:54:38 +0000

    Merged PR 183: Document Heading Height
    Document Heading Height issue in Adaptive
    
    Related work items: #2135

--- Rainmaker2/src/Presentation/admin/MCU/public/envconfig.js
+++ Rainmaker2/src/Presentation/admin/MCU/public/envconfig.js
@@ -1,5 +1,5 @@
 window.envConfig = {
-   //API_BASE_URL: 'https://qamaingateway.rainsoftfn.com',
+   // API_BASE_URL: 'https://qamaingateway.rainsoftfn.com',
    API_BASE_URL: "https://localhost:5001",
    IDLE_TIMER: '10', // Must be in minutes
    LOCK_RETAIN_DURATION: 30 // In seconds
