d58b661922177dcf59e54390512b8a2eb4e28139
danishiqbal <danish.iqbal@rainsoftfn.com>
2021-01-15 12:19:52 +0500

    increased file size and allowed number of documents
    (cherry picked from commit 1eafd24a7fa09ff8aeaf48959f6f6615051ab1a2)

--- Rainmaker2/src/Presentation/doc-manager/src/Utilities/helpers/AppEnv.ts
+++ Rainmaker2/src/Presentation/doc-manager/src/Utilities/helpers/AppEnv.ts
@@ -1,5 +1,5 @@
 export class ApplicationEnv {
   static Encode_Key: string = "RainmakerMCU2020|"; // In Mbs
-  static MaxDocumentCount: number = 10;
-  static MaxFileSize: number = 15; // In Mbs
+  static MaxDocumentCount: number = 25;
+  static MaxFileSize: number = 25; // In Mbs
 }
