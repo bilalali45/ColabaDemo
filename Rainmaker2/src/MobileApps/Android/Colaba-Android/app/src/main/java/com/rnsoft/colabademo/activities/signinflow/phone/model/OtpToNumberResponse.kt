aaa413f525166c0a2eb75dfdd1f6ba69cca9265c
USER-PC\user <irfan.adil@rainsoftfn.com>
2021-06-10 20:31:50 +0500

    Build Successful...

--- Rainmaker2/src/MobileApps/Android/Colaba-Android/app/src/main/java/com/rnsoft/colabademo/activities/signinflow/phone/model/OtpToNumberResponse.kt
+++ Rainmaker2/src/MobileApps/Android/Colaba-Android/app/src/main/java/com/rnsoft/colabademo/activities/signinflow/phone/model/OtpToNumberResponse.kt
@@ -4,12 +4,12 @@ import com.google.gson.annotations.SerializedName
 
 data class OtpToNumberResponse(
     val code: String?,
-    @SerializedName("data") val otpData: OtpData?,
+    @SerializedName("data") val otpData: OtpData2?,
     val message: String?,
     val status: String?
 )
 
-data class OtpData(
+data class OtpData2(
     val attemptsCount: Int,
     val phoneNumber: String
 )
