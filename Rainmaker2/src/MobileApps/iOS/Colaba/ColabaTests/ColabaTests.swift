b21bc29a51c11f06c106c38b2e3864d0da216d49
Muhammad Murtaza <muhammad.murtaza@rainsoftfn.com>
2021-07-07 19:03:01 +0500

    Build no increase for build

--- Rainmaker2/src/MobileApps/iOS/Colaba/ColabaTests/ColabaTests.swift
+++ Rainmaker2/src/MobileApps/iOS/Colaba/ColabaTests/ColabaTests.swift
@@ -10,17 +10,14 @@ import XCTest
 
 class ColabaTests: XCTestCase {
 
-    var sut: Validation!
-    
     override func setUpWithError() throws {
         try super.setUpWithError()
-        sut = Validation()
+        
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-        sut = nil
         try super.tearDownWithError()
     }
 
@@ -29,21 +26,18 @@ class ColabaTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
     
     func testEmail(){
         
-        do{
-            let email = try sut.validateEmail("a")
-        }
-        catch{
-            print(error.localizedDescription)
-        }
+//        let validation = Validation()
+//        let isValidEmail = validation.isValidateEmail(email: "ccc@ccc.co")
+//        XCTAssertTrue(isValidEmail, "Email is valid")
     }
 
 }
