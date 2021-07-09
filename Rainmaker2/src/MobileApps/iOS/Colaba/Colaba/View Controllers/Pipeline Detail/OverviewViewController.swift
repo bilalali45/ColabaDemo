8c90d7579d9be85d31b1b8fbb5526ef5977b5b08
Muhammad Murtaza <muhammad.murtaza@rainsoftfn.com>
2021-07-09 15:13:52 +0500

    UI work of Loan detail overview screen

--- Rainmaker2/src/MobileApps/iOS/Colaba/Colaba/View Controllers/Pipeline Detail/OverviewViewController.swift
+++ Rainmaker2/src/MobileApps/iOS/Colaba/Colaba/View Controllers/Pipeline Detail/OverviewViewController.swift
@@ -9,10 +9,49 @@ import UIKit
 
 class OverviewViewController: BaseViewController {
 
+    @IBOutlet weak var tableViewOverView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        tableViewOverView.register(UINib(nibName: "BorrowerOverviewTableViewCell", bundle: nil), forCellReuseIdentifier: "BorrowerOverviewTableViewCell")
+        tableViewOverView.register(UINib(nibName: "BorrowerAddressTableViewCell", bundle: nil), forCellReuseIdentifier: "BorrowerAddressTableViewCell")
+        tableViewOverView.register(UINib(nibName: "BorrowerLoanInfoTableViewCell", bundle: nil), forCellReuseIdentifier: "BorrowerLoanInfoTableViewCell")
+        
     }
     
+}
+
+extension OverviewViewController: UITableViewDataSource, UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if (indexPath.row == 0){
+            let cell = tableView.dequeueReusableCell(withIdentifier: "BorrowerOverviewTableViewCell", for: indexPath) as! BorrowerOverviewTableViewCell
+            return cell
+        }
+        else if (indexPath.row == 1){
+            let cell = tableView.dequeueReusableCell(withIdentifier: "BorrowerAddressTableViewCell", for: indexPath) as! BorrowerAddressTableViewCell
+            return cell
+        }
+        else{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "BorrowerLoanInfoTableViewCell", for: indexPath) as! BorrowerLoanInfoTableViewCell
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        if (indexPath.row == 2){
+            return 107
+        }
+        else{
+            return UITableView.automaticDimension
+        }
+        
+    }
 }
