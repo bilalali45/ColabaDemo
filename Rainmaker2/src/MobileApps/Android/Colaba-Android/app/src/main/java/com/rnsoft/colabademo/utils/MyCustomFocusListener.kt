866232b1cdaad178e9e087690213842526189274
anita.kiran <anita.kiran@rainsoftfn.com>
2021-08-17 18:58:09 +0500

    Added method for color change

--- Rainmaker2/src/MobileApps/Android/Colaba-Android/app/src/main/java/com/rnsoft/colabademo/utils/MyCustomFocusListener.kt
+++ Rainmaker2/src/MobileApps/Android/Colaba-Android/app/src/main/java/com/rnsoft/colabademo/utils/MyCustomFocusListener.kt
@@ -13,19 +13,17 @@ import com.rnsoft.colabademo.R
 class MyCustomFocusListener(private val mEditText: TextInputEditText, private val mTextInputLayout: TextInputLayout , private val context: Context):View.OnFocusChangeListener {
     override fun onFocusChange(p0: View?, p1: Boolean) {
         if(p1) {
-            Log.e("onFocus--","when on focus")
+            //Log.e("onFocus--","when on focus")
             setTextInputLayoutHintColor(mTextInputLayout, context = context, R.color.grey_color_two )
         }
         else{
-            Log.e("onFocus--","LOST")
+            //Log.e("onFocus--","LOST")
             if (mEditText.text?.length == 0) {
-                setTextInputLayoutHintColor(mTextInputLayout, context = context, R.color.grey_color_four )
+                setTextInputLayoutHintColor(mTextInputLayout, context = context, R.color.grey_color_three )
             } else {
                 setTextInputLayoutHintColor(mTextInputLayout, context = context, R.color.grey_color_two )
             }
         }
-
-
     }
 
     private fun setTextInputLayoutHintColor(textInputLayout: TextInputLayout, context: Context, @ColorRes colorIdRes: Int) {
