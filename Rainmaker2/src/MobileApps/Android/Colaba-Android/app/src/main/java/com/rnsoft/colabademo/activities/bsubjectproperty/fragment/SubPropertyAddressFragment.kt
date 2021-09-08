077e7d766ee9773fd95b1c9ef01b3a5b86252d7d
anita.kiran <anita.kiran@rainsoftfn.com>
2021-09-08 19:36:00 +0500

    places

--- Rainmaker2/src/MobileApps/Android/Colaba-Android/app/src/main/java/com/rnsoft/colabademo/activities/bsubjectproperty/fragment/SubPropertyAddressFragment.kt
+++ Rainmaker2/src/MobileApps/Android/Colaba-Android/app/src/main/java/com/rnsoft/colabademo/activities/bsubjectproperty/fragment/SubPropertyAddressFragment.kt
@@ -1,20 +1,45 @@
 package com.rnsoft.colabademo
 
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.AutocompletePrediction
+import com.google.android.libraries.places.api.model.AutocompleteSessionToken
+import com.google.android.libraries.places.api.model.RectangularBounds
+import com.google.android.libraries.places.api.model.TypeFilter
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsResponse
+import com.google.android.libraries.places.api.net.PlacesClient
 import com.rnsoft.colabademo.databinding.SubjectPropertyAddressBinding
 import com.rnsoft.colabademo.databinding.SubjectPropertyBinding
+import kotlinx.android.synthetic.main.temp_residence_layout.*
+import java.io.IOException
+import java.util.*
+import kotlin.collections.ArrayList
 
 /**
  * Created by Anita Kiran on 9/8/2021.
  */
-class SubPropertyAddressFragment : Fragment() {
+class SubPropertyAddressFragment : Fragment(), PlacePredictionAdapter.OnPlaceClickListener {
 
     lateinit var binding: SubjectPropertyAddressBinding
-
+    private lateinit var predictAdapter: PlacePredictionAdapter
+    private lateinit var token: AutocompleteSessionToken
+    private lateinit var placesClient: PlacesClient
+    private var predicationList: ArrayList<String> = ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,21 +48,234 @@ class SubPropertyAddressFragment : Fragment() {
     ): View? {
         binding = SubjectPropertyAddressBinding.inflate(inflater, container, false)
 
-
-
-
-
-
         binding.backButton.setOnClickListener {
             requireActivity().onBackPressed()
         }
 
-
-
+        setUpCompleteViewForPlaces()
+        initializeUSAstates()
 
         return binding.root
 
     }
+
+
+    private fun setUpCompleteViewForPlaces() {
+
+        binding.recyclerviewPlaces.apply {
+            this.setHasFixedSize(true)
+            predictAdapter = PlacePredictionAdapter(this@SubPropertyAddressFragment)
+            this.adapter = predictAdapter
+        }
+        // Create a new token for the autocomplete session. Pass this to FindAutocompletePredictionsRequest,
+        // and once again when the user makes a selection (for example when calling fetchPlace()).
+        token = AutocompleteSessionToken.newInstance()
+
+        Places.initialize(requireContext(), "AIzaSyBzPEiQOTReBzy6W1UcIyHApPu7_5Die6w")
+        // Create a new Places client instance.
+        placesClient = Places.createClient(requireContext())
+
+
+        binding.tvSearch.dropDownHeight = 0
+        binding.tvSearch.setOnFocusChangeListener { p0: View?, hasFocus: Boolean ->
+            if (hasFocus)
+                binding.tvSearch.addTextChangedListener(placeTextWatcher)
+            else
+                binding.tvSearch.removeTextChangedListener(placeTextWatcher)
+        }
+
+        binding.tvSearch.setOnClickListener { }
+    }
+
+    private val placeTextWatcher = (object : TextWatcher {
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+        override fun afterTextChanged(s: Editable) {
+            val str: String = binding.tvSearch.text.toString()
+            if (str.length >= 3) {
+                searchForGooglePlaces(str)
+            } else
+                if (str.length in 0..2) {
+                    binding.recyclerviewPlaces.visibility = View.GONE
+                    predictAdapter.setPredictions(null)
+                } else if (str.isEmpty()) {
+                    binding.recyclerviewPlaces.visibility = View.GONE
+                    predictAdapter.setPredictions(null)
+                }
+        }
+    })
+
+    private val bounds = RectangularBounds.newInstance(
+        LatLng(24.7433195, -124.7844079),
+        LatLng(49.3457868, -66.9513812)
+    )
+
+    private fun searchForGooglePlaces(queryPlace: String) {
+
+        val TAG = "SubProperty-Search"
+        binding.recyclerviewPlaces.visibility = View.VISIBLE
+
+        val request =
+            FindAutocompletePredictionsRequest.builder()
+                // Call either setLocationBias() OR setLocationRestriction().
+                .setLocationBias(bounds)
+                //.setLocationRestriction(bounds)
+                .setOrigin(LatLng(37.0902, 95.7129))
+                //s.setCountries("USA")
+                .setTypeFilter(TypeFilter.ADDRESS)
+                .setSessionToken(token)
+                .setQuery(queryPlace)
+                .build()
+
+        placesClient.findAutocompletePredictions(request)
+            .addOnSuccessListener { response: FindAutocompletePredictionsResponse ->
+                for (prediction in response.autocompletePredictions) {
+                    Log.e(TAG, prediction.placeId)
+                    response.autocompletePredictions
+                    predicationList.add(prediction.getFullText(null).toString())
+                    Log.e(TAG, prediction.getFullText(null).toString())
+
+                }
+                predictAdapter.setPredictions(response.autocompletePredictions)
+
+            }.addOnFailureListener { exception: Exception? ->
+                if (exception is ApiException) {
+                    Log.e(TAG, "Place not found: " + exception.statusCode)
+                }
+            }
+
+        Log.e("predicationList", predicationList.size.toString())
+
+    }
+
+
+    override fun onPlaceClicked(place: AutocompletePrediction) {
+        Log.e("which-place", "desc = " + place.getFullText(null).toString())
+        predictAdapter.setPredictions(null)
+        binding.searchSeparator.visibility = View.GONE
+        val placeSelected = place.getFullText(null).toString()
+        binding.tvSearch.clearFocus()
+        hideKeyBoard()
+        binding.tvSearch.removeTextChangedListener(placeTextWatcher)
+        binding.tvSearch.setText(placeSelected)
+        binding.tvSearch.clearFocus()
+
+        val geocoder = Geocoder(requireContext(), Locale.getDefault())
+        try {
+            val addresses: List<Address>? =
+                geocoder.getFromLocationName(place.getFullText(null).toString(), 1)
+            val countryName: String? = addresses?.get(0)?.countryName
+            //val stateName: String? = addresses?.get(0)?.locale
+            val locality: String? = addresses?.get(0)?.locality
+            val subLocality: String? = addresses?.get(0)?.subLocality
+            val postalCode: String? = addresses?.get(0)?.postalCode
+            val featureName: String? = addresses?.get(0)?.featureName
+            val addressLine: String? = addresses?.get(0)?.getAddressLine(0)
+            val premises: String? = addresses?.get(0)?.premises
+            val countryCode: String? = addresses?.get(0)?.countryCode
+
+
+            /*locality?.let { binding.cityEditText.setText(it) }
+            subLocality?.let { binding.countyEditText.setText(it) }
+            postalCode?.let { binding.zipcodeEditText.setText(it) }
+            countryName?.let { binding.countryCompleteTextView.setText(it) }
+            binding.streetAddressEditText.setText(place.getPrimaryText(null))
+            premises?.let { binding.unitAptInputEditText.setText(it) } */
+
+            Log.e("Bingo ", " = " + subLocality + " " + locality + "  " + postalCode)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        val extractState = place.getFullText(null)
+        var stateCode =  extractState.substring(extractState.lastIndexOf(",")-2,extractState.lastIndexOf(","))
+
+        stateCode = stateCode.capitalize()
+        Log.e("stateCode ", " = $stateCode")
+        Log.e("Test State - ", " = " +map.get("LA") +"  "+map.get(stateCode))
+
+        if(map.get(stateCode)!=null)
+            stateCompleteTextView.setText(map.get(stateCode))
+        else
+            stateCompleteTextView.setText("")
+
+      //  visibleAllFields()
+    }
+
+    /*private fun visibleAllFields() {
+        binding.cityLayout.visibility = View.VISIBLE
+        binding.countyLayout.visibility = View.VISIBLE
+        binding.countryCompleteLayout.visibility = View.VISIBLE
+        binding.zipcodeLayout.visibility = View.VISIBLE
+        binding.unitAptInputLayout.visibility = View.VISIBLE
+        binding.streetAddressLayout.visibility = View.VISIBLE
+        binding.stateCompleteTextInputLayout.visibility = View.VISIBLE
+        binding.addAddressLayout.visibility = View.VISIBLE
+
+    } */
+
+    private fun hideKeyBoard() {
+
+    }
+
+    private var map: HashMap<String, String> = HashMap()
+
+    private fun initializeUSAstates() {
+        map.put("AL" , "Alabama")
+        map.put("AK", "Alaska")
+        map.put("AZ", "Arizona")
+        map.put("AR", "Arkansas")
+        map.put("CA", "California")
+        map.put("CO", "Colorado")
+        map.put("CT", "Connecticut")
+        map.put("DE", "Delaware")
+        map.put("DC", "District of Columbia")
+        map.put("FL", "Florida")
+        map.put("GA", "Georgia")
+        map.put("HI", "Hawaii")
+        map.put("ID", "Idaho")
+        map.put("IL", "Illinois")
+        map.put("IN", "Indiana")
+        map.put("IA", "Iowa")
+        map.put("KS", "Kansas")
+        map.put("KY", "Kentucky")
+        map.put("LA", "Louisiana")
+        map.put("ME", "Maine")
+        map.put("MD", "Maryland")
+        map.put("MA", "Massachusetts")
+        map.put("MI", "Michigan")
+        map.put("MN", "Minnesota")
+        map.put("MS", "Mississippi")
+        map.put("MO", "Missouri")
+        map.put("MT", "Montana")
+        map.put("NE", "Nebraska")
+        map.put("NV", "Nevada")
+        map.put("NH", "New Hampshire")
+        map.put("NJ", "New Jersey")
+        map.put("NM", "New Mexico")
+        map.put("NY", "New York")
+        map.put("NC", "North Carolina")
+        map.put("ND", "North Dakota")
+        map.put("OH", "Ohio")
+        map.put("OK", "Oklahoma")
+        map.put("OR", "Oregon")
+        map.put("PA", "Pennsylvania")
+        map.put("RI", "Rhode Island")
+        map.put("SC", "South Carolina")
+        map.put("SD", "South Dakota")
+        map.put("TN", "Tennessee")
+        map.put("TX", "Texas")
+        map.put("UT", "Utah")
+        map.put("VT", "Vermont")
+        map.put("VA", "Virginia")
+        map.put("WA", "Washington")
+        map.put("WV", "West Virginia")
+        map.put("WI", "Wisconsin")
+        map.put("WY", "Wyoming")
+    }
+
+
+
 
 
 
