e82e214303bc369d1f753e2d44076de9382705d4
USER-PC\user <irfan.adil@rainsoftfn.com>
2021-10-01 19:54:35 +0500

    new screen added

---
+++ Rainmaker2/src/MobileApps/Android/Colaba-Android/app/src/main/java/com/rnsoft/colabademo/activities/requestdocs/RequestDocsActivity.kt
@@ -0,0 +1,30 @@
+package com.rnsoft.colabademo
+
+import android.content.SharedPreferences
+import android.os.Bundle
+import androidx.navigation.findNavController
+import com.rnsoft.colabademo.databinding.RequestDocsActivityLayoutBinding
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class RequestDocsActivity : BaseActivity() {
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
+    private lateinit var binding: RequestDocsActivityLayoutBinding
+    var loanApplicationId:Int? = null
+    var loanPurpose:String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = RequestDocsActivityLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        overridePendingTransition(R.anim.slide_in_right, R.anim.hold)
+        val extras = intent.extras
+        extras?.let {
+            loanApplicationId = it.getInt(AppConstant.loanApplicationId)
+            loanPurpose = it.getString(AppConstant.loanPurpose)
+        }
+        val navController = findNavController(R.id.nav_host_borrower_loan)
+    }
+}
