3ddae4004c7f91953584b3b5881192ab0d682d23
Muhammad Murtaza <muhammad.murtaza@rainsoftfn.com>
2021-10-27 13:31:04 +0500

    Demographic work (In Progress)

--- Rainmaker2/src/MobileApps/iOS/Colaba/Colaba/AppDelegate.swift
+++ Rainmaker2/src/MobileApps/iOS/Colaba/Colaba/AppDelegate.swift
@@ -90,27 +90,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func showInitialViewController(){
             
-        var isAlreadyRegisteredWithBiometric = ""
-        if let isBiometricRegistered = UserDefaults.standard.value(forKey: kIsUserRegisteredWithBiometric){
-            isAlreadyRegisteredWithBiometric = isBiometricRegistered as! String
-        }
-
-        if (isAlreadyRegisteredWithBiometric == kYes && UserModel.getCurrentUser() != nil){
-            if (Utility.checkDeviceAuthType() == kTouchID){
-                loadFingerPrintViewController()
-            }
-            else if (Utility.checkDeviceAuthType() == kFaceID){
-                loadFaceLockViewController()
-            }
-            else{
-                loadLoginViewController()
-            }
-        }
-        else{
-            loadLoginViewController()
-        }
-//        self.loadBorrowerInfoController()
-//        self.window?.makeKeyAndVisible()
+//        var isAlreadyRegisteredWithBiometric = ""
+//        if let isBiometricRegistered = UserDefaults.standard.value(forKey: kIsUserRegisteredWithBiometric){
+//            isAlreadyRegisteredWithBiometric = isBiometricRegistered as! String
+//        }
+//
+//        if (isAlreadyRegisteredWithBiometric == kYes && UserModel.getCurrentUser() != nil){
+//            if (Utility.checkDeviceAuthType() == kTouchID){
+//                loadFingerPrintViewController()
+//            }
+//            else if (Utility.checkDeviceAuthType() == kFaceID){
+//                loadFaceLockViewController()
+//            }
+//            else{
+//                loadLoginViewController()
+//            }
+//        }
+//        else{
+//            loadLoginViewController()
+//        }
+        self.loadBorrowerInfoController()
+        self.window?.makeKeyAndVisible()
     }
 
     func loadDashboardViewController(){
@@ -119,9 +119,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func loadBorrowerInfoController(){
-        let vc = Utility.getSendDocumentRequestVC()
-        //let vc = Utility.getTestVC()
-        self.window?.rootViewController = vc
+//        let vc = Utility.getSendDocumentRequestVC()
+//        //let vc = Utility.getTestVC()
+//        self.window?.rootViewController = vc
+        
+        let vc = Utility.getLoanDetailVC()
+        vc.loanApplicationId = 1050
+        vc.borrowerName = "Muhammad Farhan"
+        vc.loanPurpose = "Purchase"
+        vc.phoneNumber = ""
+        vc.email = "farhan@mailinator.com"
+        let navVC = UINavigationController(rootViewController: vc)
+        navVC.navigationBar.isHidden = true
+        navVC.modalPresentationStyle = .fullScreen
+        self.window?.rootViewController = navVC
     }
     
     func loadLoginViewController(){
