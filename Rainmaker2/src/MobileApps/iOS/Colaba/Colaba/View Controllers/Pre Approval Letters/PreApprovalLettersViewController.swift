c26f4d7642738e2ae8414062ccb388c534cb9b3c
Muhammad Murtaza <muhammad.murtaza@rainsoftfn.com>
2021-12-13 16:05:19 +0500

    Add File Pre Approval Letter

---
+++ Rainmaker2/src/MobileApps/iOS/Colaba/Colaba/View Controllers/Pre Approval Letters/PreApprovalLettersViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PreApprovalLettersViewController.swift
+//  Colaba
+//
+//  Created by Muhammad Murtaza on 13/12/2021.
+//
+
+import UIKit
+
+class PreApprovalLettersViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
